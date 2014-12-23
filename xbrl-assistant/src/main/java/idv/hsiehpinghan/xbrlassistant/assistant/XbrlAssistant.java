bb08b4eedfae35fb78d7bd8b6ff6860a8e7de594
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2014-12-23 18:02:21 +0800

    Backup.

--- xbrl-assistant/src/main/java/idv/hsiehpinghan/xbrlassistant/assistant/XbrlAssistant.java
+++ xbrl-assistant/src/main/java/idv/hsiehpinghan/xbrlassistant/assistant/XbrlAssistant.java
@@ -16,11 +16,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import jcx.xbrl.data.XbrlContext;
-import jcx.xbrl.data.XbrlContextScenario;
 import jcx.xbrl.data.XbrlDocument;
 import jcx.xbrl.data.XbrlElement;
-import jcx.xbrl.data.XbrlEntity;
 import jcx.xbrl.data.XbrlPeriod;
 import jcx.xbrl.data.XbrlTreeNode;
 import jcx.xbrl.taxonomy.XbrlPresentationTree;
@@ -34,27 +31,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Component
-public class XbrlAssistant {	
+public class XbrlAssistant {
 	public static final String EN = "en";
+	// private Logger logger = Logger.getLogger(this.getClass().getName());
 	@Autowired
 	private ObjectMapper objectMapper;
 	@Autowired
 	private XbrlTaxonomy tifrsCiCr20130331;
-	// Presentation id
-	// public static final String BALANCESHEET_PRESENTATION_ID =
-	// "http://www.xbrl.org/tifrs/fr/role/BalanceSheet";
 
-	// public static final String ELEMENT_ID = "elementId";
-
-	
-
-	// private Logger logger = Logger.getLogger(this.getClass().getName());
-
-
-	public ObjectNode getPresentationJson(File instanceFile, List<String> presentIds) throws Exception{
+	/**
+	 * Get presentation report as json format. (PresentIds can reference Presentation.Id....) 
+	 * @param instanceFile
+	 * @param presentIds
+	 * @return
+	 * @throws Exception
+	 */
+	public ObjectNode getPresentationJson(File instanceFile,
+			List<String> presentIds) throws Exception {
 		XbrlTaxonomy taxonomy = getXbrlTaxonomy(instanceFile);
 		XbrlDocument document = loadXbrlDocument(instanceFile);
-		
+
 		@SuppressWarnings("unchecked")
 		Vector<String> presents = taxonomy.getPresentationList();
 		ObjectNode objNode = objectMapper.createObjectNode();
@@ -62,7 +58,7 @@ public class XbrlAssistant {
 			// Get presentation node roleURI. (ex :
 			// http://www.xbrl.org/tifrs/fr/role/BalanceSheet)
 			String presentId = presents.get(i);
-			if(presentIds != null && presentIds.contains(presentId) == false) {
+			if (presentIds.contains(presentId) == false) {
 				continue;
 			}
 			ObjectNode presentNod = objectMapper.createObjectNode();
@@ -73,65 +69,16 @@ public class XbrlAssistant {
 				// Get root node. (ex :
 				// ifrs_StatementOfComprehensiveIncomeAbstract)
 				XbrlTreeNode rootNode = tree.getRootNode();
-				generateJsonObjectContent(presentNod, rootNode, taxonomy, document);
+				generateJsonObjectContent(presentNod, rootNode, taxonomy,
+						document);
 			}
 		}
 
 		return objNode;
 	}
 
-	private void generateJsonObjectContent(ObjectNode parentObjNode, XbrlTreeNode treeNode,
-			XbrlTaxonomy taxonomy, XbrlDocument document) throws Exception {
-		String elementId = treeNode.getID();
-		ObjectNode objNode = objectMapper.createObjectNode();
-		parentObjNode.set(elementId, objNode);
-		objNode.put(Presentation.Attribute.CHINESE_LABEL, taxonomy.getLabelByID(elementId));
-		objNode.put(Presentation.Attribute.ENGLISH_LABEL, taxonomy.getLabelByID(elementId, EN));
-		objNode.put(Presentation.Attribute.ORDER, treeNode.getOrder());
-		generateJsonValueContent(document, elementId, objNode);
-		if (treeNode.hasChild()) {
-			XbrlTreeNode childTreeNode = treeNode.getFirstChild();
-			do {
-				ObjectNode childObjNode = objectMapper.createObjectNode();
-				String childElementId = childTreeNode.getID();
-				objNode.set(childElementId, childObjNode);
-				generateJsonObjectContent(childObjNode, childTreeNode, taxonomy, document);
-				childTreeNode = childTreeNode.getNextSibling();
-			} while (childTreeNode != null);
-		}
-	}
-	
-	private void generateJsonValueContent(XbrlDocument document, String elementId, ObjectNode objNode) {
-		ObjectNode valuesObjNode = objectMapper.createObjectNode();
-		XbrlElement[] eles = document.getAllItems(elementId);
-		if(eles.length <= 0) {
-			return;
-		}
-		for(XbrlElement ele : eles) {
-			ObjectNode valueObjNode = objectMapper.createObjectNode();
-			valueObjNode.put(Instance.Attribute.VALUE, ele.getValue());
-			valueObjNode.put(Instance.Attribute.UNIT, ele.getUnit());
-			setPeriodValue(ele, valueObjNode);
-			valuesObjNode.set(ele.getContext().getID(), valueObjNode);
-		}
-		objNode.set(Instance.Customization.VALUES, valuesObjNode);
-	}
-	
-	private void setPeriodValue(XbrlElement element, ObjectNode valueObjNode) {
-		XbrlPeriod period = element.getContext().getPeriod();
-		String periodType = period.getPeriodType();
-		valueObjNode.put(Instance.Attribute.PERIOD_TYPE, periodType);
-		if(Instance.Attribute.INSTANT.equals(periodType)) {
-			valueObjNode.put(Instance.Attribute.INSTANT, period.getInstantDateString());
-		} else if(Instance.Attribute.DURATION.equals(periodType)) {
-			valueObjNode.put(Instance.Attribute.START_DATE, period.getStartDateString());
-			valueObjNode.put(Instance.Attribute.END_DATE, period.getEndDateString());
-		} else {
-			throw new RuntimeException("Unknown period type !!!");
-		}
-	}
-	
-	XbrlTaxonomy getXbrlTaxonomy(File instanceFile) throws ParserConfigurationException, SAXException, IOException {
+	XbrlTaxonomy getXbrlTaxonomy(File instanceFile)
+			throws ParserConfigurationException, SAXException, IOException {
 		XbrlTaxonomyVersion version = getXbrlTaxonomyVersion(instanceFile);
 		switch (version) {
 		case TIFRS_CI_CR_2013_03_31:
@@ -139,13 +86,14 @@ public class XbrlAssistant {
 		}
 		throw new RuntimeException("XbrlTaxonomy version undefined");
 	}
-	
+
 	XbrlDocument loadXbrlDocument(File instanceFile) throws Exception {
 		XbrlDocument xDoc = new XbrlDocument();
-		xDoc.load(getXbrlTaxonomy(instanceFile), new FileInputStream(instanceFile));
+		xDoc.load(getXbrlTaxonomy(instanceFile), new FileInputStream(
+				instanceFile));
 		return xDoc;
 	}
-	
+
 	XbrlTaxonomyVersion getXbrlTaxonomyVersion(File instanceFile)
 			throws ParserConfigurationException, SAXException, IOException {
 		SAXParser saxParser;
@@ -158,41 +106,66 @@ public class XbrlAssistant {
 		} catch (SaxParserBreakException e) {
 			return handler.getXbrlTaxonomyVersion();
 		}
-		return null;
+		throw new RuntimeException("Xbrl taxonomy version not found !!!");
+	}
+	
+	private void generateJsonObjectContent(ObjectNode parentObjNode,
+			XbrlTreeNode treeNode, XbrlTaxonomy taxonomy, XbrlDocument document)
+			throws Exception {
+		String elementId = treeNode.getID();
+		ObjectNode objNode = objectMapper.createObjectNode();
+		parentObjNode.set(elementId, objNode);
+		objNode.put(Presentation.Attribute.CHINESE_LABEL,
+				taxonomy.getLabelByID(elementId));
+		objNode.put(Presentation.Attribute.ENGLISH_LABEL,
+				taxonomy.getLabelByID(elementId, EN));
+		objNode.put(Presentation.Attribute.ORDER, treeNode.getOrder());
+		generateJsonValueContent(document, elementId, objNode);
+		if (treeNode.hasChild()) {
+			XbrlTreeNode childTreeNode = treeNode.getFirstChild();
+			do {
+				ObjectNode childObjNode = objectMapper.createObjectNode();
+				String childElementId = childTreeNode.getID();
+				objNode.set(childElementId, childObjNode);
+				generateJsonObjectContent(childObjNode, childTreeNode,
+						taxonomy, document);
+				childTreeNode = childTreeNode.getNextSibling();
+			} while (childTreeNode != null);
+		}
 	}
 
+	private void generateJsonValueContent(XbrlDocument document,
+			String elementId, ObjectNode objNode) {
+		ObjectNode valuesObjNode = objectMapper.createObjectNode();
+		XbrlElement[] eles = document.getAllItems(elementId);
+		if (eles.length <= 0) {
+			return;
+		}
+		for (XbrlElement ele : eles) {
+			ObjectNode valueObjNode = objectMapper.createObjectNode();
+			valueObjNode.put(Instance.Attribute.VALUE, ele.getValue());
+			valueObjNode.put(Instance.Attribute.UNIT, ele.getUnit());
+			setPeriodValue(ele, valueObjNode);
+			valuesObjNode.set(ele.getContext().getID(), valueObjNode);
+		}
+		objNode.set(Instance.Customization.VALUES, valuesObjNode);
+	}
 
+	private void setPeriodValue(XbrlElement element, ObjectNode valueObjNode) {
+		XbrlPeriod period = element.getContext().getPeriod();
+		String periodType = period.getPeriodType();
+		valueObjNode.put(Instance.Attribute.PERIOD_TYPE, periodType);
+		if (Instance.Attribute.INSTANT.equals(periodType)) {
+			valueObjNode.put(Instance.Attribute.INSTANT,
+					period.getInstantDateString());
+		} else if (Instance.Attribute.DURATION.equals(periodType)) {
+			valueObjNode.put(Instance.Attribute.START_DATE,
+					period.getStartDateString());
+			valueObjNode.put(Instance.Attribute.END_DATE,
+					period.getEndDateString());
+		} else {
+			throw new RuntimeException("Unknown period type !!!");
+		}
+	}
 
-
-//	public void test(XbrlTaxonomy taxonomy) throws Exception {
-//		@SuppressWarnings("unchecked")
-//		Vector<String> presents = taxonomy.getPresentationList();
-//		for (int i = 0, size = presents.size(); i < size; ++i) {
-//			// Get presentation node roleURI. (ex :
-//			// http://www.xbrl.org/tifrs/fr/role/BalanceSheet)
-//			String presentNode = presents.get(i);
-//			XbrlPresentationTree[] presentTrees = taxonomy
-//					.getPresentationTree(presentNode);
-//			for (XbrlPresentationTree tree : presentTrees) {
-//				// Get root node. (ex :
-//				// ifrs_StatementOfComprehensiveIncomeAbstract)
-//				XbrlTreeNode rootNode = tree.getRootNode();
-//				getSubNodes(rootNode, taxonomy, "");
-//			}
-//
-//		}
-//	}
-//
-//	private void getSubNodes(XbrlTreeNode node, XbrlTaxonomy taxonomy, String s)
-//			throws Exception {
-//		System.err.println(node.getID() + " / " + s
-//				+ taxonomy.getLabelByID(node.getID()));
-//		if (node.hasChild()) {
-//			XbrlTreeNode child = node.getFirstChild();
-//			do {
-//				getSubNodes(child, taxonomy, s + " ");
-//				child = child.getNextSibling();
-//			} while (child != null);
-//		}
-//	}
 }
--- xbrl-assistant/src/test/java/idv/hsiehpinghan/xbrlassistant/assistant/XbrlAssistantTest.java
+++ xbrl-assistant/src/test/java/idv/hsiehpinghan/xbrlassistant/assistant/XbrlAssistantTest.java
@@ -20,7 +20,6 @@ import jcx.xbrl.data.XbrlElement;
 import jcx.xbrl.taxonomy.XbrlTaxonomy;
 import junit.framework.Assert;
 
-import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -37,8 +36,6 @@ public class XbrlAssistantTest {
 	private XbrlDocument document;
 	private ObjectMapper objectMapper;
 
-	private Logger logger = Logger.getLogger(this.getClass().getName());
-	
 	@BeforeClass
 	public void beforeClass() {
 		ApplicationContext applicationContext = TestngSuitSetting
@@ -46,29 +43,32 @@ public class XbrlAssistantTest {
 		xbrlAssistant = applicationContext.getBean(XbrlAssistant.class);
 		tifrsCiCr20130331 = applicationContext.getBean(XbrlTaxonomy.class);
 		objectMapper = applicationContext.getBean(ObjectMapper.class);
-		
+
 		String instancePath = "xbrl-instance/2013-01-sii-01-C/tifrs-fr0-m1-ci-cr-1101-2013Q1.xml";
 		instanceFile = ResourceUtility.getFileResource(instancePath);
 	}
-	
-//	@Test
-	public void getXbrlTaxonomyVersion() throws ParserConfigurationException, SAXException, IOException {
-		XbrlTaxonomyVersion version = xbrlAssistant.getXbrlTaxonomyVersion(instanceFile);
+
+	 @Test
+	public void getXbrlTaxonomyVersion() throws ParserConfigurationException,
+			SAXException, IOException {
+		XbrlTaxonomyVersion version = xbrlAssistant
+				.getXbrlTaxonomyVersion(instanceFile);
 		Assert.assertEquals(XbrlTaxonomyVersion.TIFRS_CI_CR_2013_03_31, version);
 	}
-	
-//	@Test
-	public void getXbrlTaxonomy() throws ParserConfigurationException, SAXException, IOException {
+
+	 @Test
+	public void getXbrlTaxonomy() throws ParserConfigurationException,
+			SAXException, IOException {
 		XbrlTaxonomy taxonomy = xbrlAssistant.getXbrlTaxonomy(instanceFile);
 		Assert.assertSame(taxonomy, tifrsCiCr20130331);
 	}
 
-//	@Test
+	 @Test
 	public void loadXbrlDocument() throws Exception {
 		document = xbrlAssistant.loadXbrlDocument(instanceFile);
 		Assert.assertTrue(document.getAllContext().size() == 89);
 	}
-	
+
 	@Test
 	public void getPresentationJson() throws Exception {
 		List<String> presentationIds = new ArrayList<String>(4);
@@ -76,102 +76,36 @@ public class XbrlAssistantTest {
 		presentationIds.add(Presentation.Id.StatementOfComprehensiveIncome);
 		presentationIds.add(Presentation.Id.StatementOfCashFlows);
 		presentationIds.add(Presentation.Id.StatementOfChangesInEquity);
-		ObjectNode objNode = xbrlAssistant.getPresentationJson(instanceFile, presentationIds);
+		ObjectNode objNode = xbrlAssistant.getPresentationJson(instanceFile,
+				presentationIds);
 		// Balance sheet test
 		JsonNode balanceSheetNode = objNode.get(Presentation.Id.BalanceSheet);
-		JsonNode blanceSheetSample = objectMapper.readTree(ResourceUtility.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_BalanceSheet.json"));
-		Assert.assertEquals(balanceSheetNode, blanceSheetSample);	
+		JsonNode blanceSheetSample = objectMapper
+				.readTree(ResourceUtility
+						.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_BalanceSheet.json"));
+		Assert.assertEquals(balanceSheetNode, blanceSheetSample);
 		// Statement of comprehensive income test
-		JsonNode incomeNode = objNode.get(Presentation.Id.StatementOfComprehensiveIncome);
-		JsonNode incomeSample = objectMapper.readTree(ResourceUtility.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_StatementOfComprehensiveIncome.json"));
+		JsonNode incomeNode = objNode
+				.get(Presentation.Id.StatementOfComprehensiveIncome);
+		JsonNode incomeSample = objectMapper
+				.readTree(ResourceUtility
+						.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_StatementOfComprehensiveIncome.json"));
 		Assert.assertEquals(incomeNode, incomeSample);
 		// Statement of cash flows test
-		JsonNode cashFlowNode = objNode.get(Presentation.Id.StatementOfCashFlows);
-		JsonNode cashFlowSample = objectMapper.readTree(ResourceUtility.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_StatementOfCashFlows.json"));
+		JsonNode cashFlowNode = objNode
+				.get(Presentation.Id.StatementOfCashFlows);
+		JsonNode cashFlowSample = objectMapper
+				.readTree(ResourceUtility
+						.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_StatementOfCashFlows.json"));
 		Assert.assertEquals(cashFlowNode, cashFlowSample);
 		// Statement of changes in equity test
-		JsonNode equityChangeNode = objNode.get(Presentation.Id.StatementOfChangesInEquity);
-		JsonNode equityChangeSample = objectMapper.readTree(ResourceUtility.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_StatementOfChangesInEquity.json"));
+		JsonNode equityChangeNode = objNode
+				.get(Presentation.Id.StatementOfChangesInEquity);
+		JsonNode equityChangeSample = objectMapper
+				.readTree(ResourceUtility
+						.getFileResource("sample/tifrs-fr0-m1-ci-cr-1101-2013Q1_StatementOfChangesInEquity.json"));
 		Assert.assertEquals(equityChangeNode, equityChangeSample);
 	}
-//	@Test
-//	public void getXbrlTaxonomy() throws Exception {
-//		String taxonomyPath = "xbrl-taxonomy/tifrs-20130331/XBRL_TW_Entry_Points/CI/CR/tifrs-ci-cr-2013-03-31.xsd";
-//		taxonomy = xbrlAssistant.getXbrlTaxonomy(taxonomyPath);
-//		@SuppressWarnings("unchecked")
-//		Vector<String> schemaList = taxonomy.getSchemaList();
-//		// print element id.(ex : tifrs-bsci-ci_LongtermNotesPayable)
-//		// print(schemaList);
-//		Assert.assertTrue(schemaList.size() == 3926);
-//	}
-//
-//
-//	@Test(dependsOnMethods = { "loadXbrlDocument" })
-//	public void getBalanceSheet() throws a {
-//		@SuppressWarnings("unchecked")
-//		Vector<String> presents = taxonomy.getPresentationList();
-//		for (int i = 0, size = presents.size(); i < size; ++i) {
-//			// Get presentation node roleURI. (ex :
-//			// http://www.xbrl.org/tifrs/fr/role/BalanceSheet)
-//			String presentNode = presents.get(i);
-//			XbrlPresentationTree[] presentTrees = taxonomy
-//					.getPresentationTree(presentNode);
-//			for (XbrlPresentationTree tree : presentTrees) {
-//				// Get root node. (ex :
-//				// ifrs_StatementOfComprehensiveIncomeAbstract)
-//				XbrlTreeNode rootNode = tree.getRootNode();
-////				getSubNodes(rootNode, "");
-//			}
-//
-//		}
-//		
-//		
-////		ObjectMapper mapper = new ObjectMapper();
-////		ObjectNode rootNode = mapper.createObjectNode();
-////		return rootNode;
-//	}
-//	
-////	@Test(dependsOnMethods = { "getXbrlTaxonomy" })
-//	public void test() throws Exception {
-//
-//		// XbrlElement ele
-//		// =document.getElementByID("ifrs_NoncontrollingInterests",
-//		// "AsOf20130331");
-//		// System.err.println(ele.getValue());
-//
-//		// @SuppressWarnings("unchecked")
-//		// Hashtable<String, XbrlElementPool> hashTable =
-//		// document.getElements();
-//		// print(hashTable);
-//
-//		@SuppressWarnings("unchecked")
-//		Vector<String> presents = taxonomy.getPresentationList();
-//		for (int i = 0, size = presents.size(); i < size; ++i) {
-//			// Get presentation node roleURI. (ex :
-//			// http://www.xbrl.org/tifrs/fr/role/BalanceSheet)
-//			String presentNode = presents.get(i);
-//			XbrlPresentationTree[] presentTrees = taxonomy
-//					.getPresentationTree(presentNode);
-//			for (XbrlPresentationTree tree : presentTrees) {
-//				// Get root node. (ex :
-//				// ifrs_StatementOfComprehensiveIncomeAbstract)
-//				XbrlTreeNode rootNode = tree.getRootNode();
-//				getSubNodes(rootNode, "");
-//			}
-//
-//		}
-//	}
-//
-//	private void getSubNodes(XbrlTreeNode node, String s) throws Exception {
-//		System.err.println(node.getID() + " / " + s + taxonomy.getLabelByID(node.getID()));
-////		if (node.hasChild()) {
-////			XbrlTreeNode child = node.getFirstChild();
-////			do {
-////				getSubNodes(child, s + " ");
-////				child = child.getNextSibling();
-////			} while (child != null);
-////		}
-//	}
 
 	@SuppressWarnings("unused")
 	private void print(Vector<?> vec) {
@@ -189,53 +123,14 @@ public class XbrlAssistantTest {
 			System.err.println(key + " / " + value);
 		}
 	}
-	
+
 	@SuppressWarnings("unused")
 	private void print(XbrlElement[] elements) {
-		for(int i = 0, size = elements.length; i < size; ++i) {
+		for (int i = 0, size = elements.length; i < size; ++i) {
 			XbrlElement ele = elements[i];
-			System.err.println(ele.getID() + " / " + ele.getContext().getID() + " / " + ele.getValue());
+			System.err.println(ele.getID() + " / " + ele.getContext().getID()
+					+ " / " + ele.getValue());
 		}
 	}
-//	
-//	public static void main(String[] args) throws Exception {
-//
-//		SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-//		File file = ResourceUtility.getFileResource("xbrl-instance/2013-01-sii-01-C/tifrs-fr0-m1-ci-cr-1101-2013Q1.xml");
-//		saxParser.parse(file, new SchemaReferenceHandler());
-////	    SAXReader reader = new SAXReader();
-////	    File file = ResourceUtility.getFileResource("xbrl-instance/2013-01-sii-01-C/tifrs-fr0-m1-ci-cr-1101-2013Q1.xml");
-////	    
-////
-////        System.err.println(file.exists());
-////        
-////        Document doc = reader.read(file);
-////        Element ele = doc.getRootElement();
-////        
-////        List list = doc.selectNodes("context");
-//
-//
-////        System.err.println(list.size());
-//        
-////		XbrlTaxonomy taxonomy = xbrlAssistant.getXbrlTaxonomy("xbrl-taxonomy/tifrs-20130331/XBRL_TW_Entry_Points/CI/CR/tifrs-ci-cr-2013-03-31.xsd");
-////		XbrlDocument document = xbrlAssistant.loadXbrlDocument(taxonomy, "xbrl-instance/2013-01-sii-01-C/tifrs-fr0-m1-ci-cr-1101-2013Q1.xml") ;
-////		ObjectMapper mapper = new ObjectMapper();
-////		ObjectNode objNode = xbrlAssistant.getPresentationMap(taxonomy, mapper);
-//		
-//
-////		System.err.println(objNode);
-//		
-//
-////		XbrlUtility.test(taxonomy);
-//		
-////		Vector<XbrlContext> contexts = document.getAllContext();
-////        for(int j = 0, sz = contexts.size(); j < sz; ++j) {
-////        	System.out.println(contexts.get(j).getDimensionID());
-////        	System.out.println(contexts.get(j).getDimensionMember());
-////        	System.out.println(contexts.get(j).getID());
-////        	System.out.println(contexts.get(j).getPeriodType());
-////        	System.out.println(contexts.get(j).hasDimension());
-//////        	System.out.println(contexts.get(j).);
-////        }
-//	}
+
 }

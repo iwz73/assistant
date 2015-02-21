67592acd25f70fe6063d588090a109fce25cefeb
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-02-22 01:37:49 +0800

    Backup.

--- hbase-assistant/src/main/java/idv/hsiehpinghan/hbaseassistant/utility/HBaseClassGenerateUtility.java
+++ hbase-assistant/src/main/java/idv/hsiehpinghan/hbaseassistant/utility/HBaseClassGenerateUtility.java
@@ -32,7 +32,7 @@ public class HBaseClassGenerateUtility {
 
 	public static void main(String[] args) throws IOException {
 		File f = new File(
-				"/home/centos/git/dao/stock-dao/src/test/entity-json/DailyData.json");
+				"/home/centos/git/dao/stock-dao/src/test/entity-json/MonthlyData.json");
 		parseJson(f);
 		String classCode = getEntityClassCode();
 		System.err.println("entity : " + classCode);

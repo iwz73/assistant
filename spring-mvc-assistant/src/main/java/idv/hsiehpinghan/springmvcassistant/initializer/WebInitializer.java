3b4854ce69f668b4bb62dc1bb32ce27a6b096124
thank <thank@localhost>
2015-07-19 20:50:11 -0700

    backup.

--- spring-mvc-assistant/src/main/java/idv/hsiehpinghan/springmvcassistant/initializer/WebInitializer.java
+++ spring-mvc-assistant/src/main/java/idv/hsiehpinghan/springmvcassistant/initializer/WebInitializer.java
@@ -1,71 +1,51 @@
 package idv.hsiehpinghan.springmvcassistant.initializer;
 
-import javax.servlet.FilterRegistration;
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebInitializer implements WebApplicationInitializer {
+public class WebInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
-	public void onStartup(final ServletContext servletContext)
-			throws ServletException {
-		// registerListener(servletContext);
-		registerDispatcherServlet(servletContext);
-		registerCharacterEncodingFilter(servletContext);
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
 	}
 
-	private void registerListener(final ServletContext servletContext) {
-		AnnotationConfigWebApplicationContext applicationContext = createApplicationContext();
-		ContextLoaderListener contextLoaderListener = new ContextLoaderListener(
-				applicationContext);
-		servletContext.addListener(contextLoaderListener);
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { idv.hsiehpinghan.springmvcassistant.configuration.SpringConfiguration.class };
 	}
 
-	private void registerDispatcherServlet(ServletContext servletContext) {
-		AnnotationConfigWebApplicationContext applicationContext = createApplicationContext(idv.hsiehpinghan.springmvcassistant.configuration.SpringConfiguration.class);
-		DispatcherServlet dispatcherServlet = new DispatcherServlet(
-				applicationContext);
-		ServletRegistration.Dynamic registration = servletContext.addServlet(
-				"dispatcherServlet", dispatcherServlet);
-		registration.addMapping("/");
-		registration.setLoadOnStartup(1);
-		setMultipartConfigElement(registration);
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
 	}
 
-	private void setMultipartConfigElement(
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return new Filter[] { characterEncodingFilter };
+	}
+
+	@Override
+	protected void customizeRegistration(
 			ServletRegistration.Dynamic registration) {
+		registration.setMultipartConfig(generateMultipartConfigElement());
+	}
+
+	private MultipartConfigElement generateMultipartConfigElement() {
 		String location = null;
 		long maxFileSize = 1024 * 1024 * 5;
 		long maxRequestSize = -1L;
 		int fileSizeThreshold = 1024 * 1024;
-		MultipartConfigElement config = new MultipartConfigElement(location,
-				maxFileSize, maxRequestSize, fileSizeThreshold);
-		registration.setMultipartConfig(config);
-	}
-	
-	private void registerCharacterEncodingFilter(ServletContext servletContext) {
-		FilterRegistration.Dynamic characterEncodingFilter = servletContext
-				.addFilter("characterEncodingFilter",
-						new CharacterEncodingFilter());
-		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
-		characterEncodingFilter.setInitParameter("forceEncoding", "true");
-		characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
-	}
-
-	private AnnotationConfigWebApplicationContext createApplicationContext(
-			final Class<?>... annotationClasses) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-		applicationContext.getEnvironment().setActiveProfiles("local");
-		applicationContext.register(annotationClasses);
-		return applicationContext;
+		return new MultipartConfigElement(location, maxFileSize,
+				maxRequestSize, fileSizeThreshold);
 	}
 
 }

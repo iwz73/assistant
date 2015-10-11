2d5fc7b73aa9bd6e9adbf4dca5f99e434e5d16b1
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-10-12 07:47:13 +0800

    backup.

---
+++ gora-assistant/src/test/java/idv/hsiehpinghan/goraassistant/configuration/SpringConfigurationTest.java
@@ -0,0 +1,22 @@
+package idv.hsiehpinghan.goraassistant.configuration;
+
+import idv.hsiehpinghan.goraassistant.entity.Gora;
+
+import org.apache.gora.store.DataStore;
+import org.apache.gora.store.DataStoreFactory;
+import org.apache.gora.util.GoraException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("goraAssistantSpringConfigurationTest")
+@ComponentScan(basePackages = { "idv.hsiehpinghan.goraassistant" })
+public class SpringConfigurationTest {
+	// private Logger logger = Logger.getLogger(this.getClass().getName());
+
+	@Bean
+	public DataStore<Long, Gora> dataStore() throws GoraException {
+		return DataStoreFactory.getDataStore(Long.class, Gora.class,
+				new org.apache.hadoop.conf.Configuration());
+	}
+}

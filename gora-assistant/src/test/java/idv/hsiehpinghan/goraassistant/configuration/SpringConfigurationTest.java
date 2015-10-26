77378adca075eed4edd71fc52b8882abfdd743b2
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2015-10-26 23:12:15 +0800

    backup.

--- gora-assistant/src/test/java/idv/hsiehpinghan/goraassistant/configuration/SpringConfigurationTest.java
+++ gora-assistant/src/test/java/idv/hsiehpinghan/goraassistant/configuration/SpringConfigurationTest.java
@@ -10,7 +10,6 @@ import org.apache.nutch.util.NutchConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration("goraAssistantSpringConfigurationTest")
 @ComponentScan(basePackages = { "idv.hsiehpinghan.goraassistant" })

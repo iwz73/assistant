578638c98b500fc5b23ae58308bb97c2a7713eca
thank <thank@localhost>
2016-02-11 18:16:15 +0800

    backup.

--- spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/configuration/SpringConfiguration.java
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/configuration/SpringConfiguration.java
@@ -1,6 +1,7 @@
 package idv.hsiehpinghan.springbatchassistant.configuration;
 
-import java.util.Arrays;
+import java.beans.PropertyVetoException;
+import java.sql.SQLException;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
@@ -8,29 +9,52 @@ import javax.sql.DataSource;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.jdbc.datasource.init.ScriptException;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
-@Configuration
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @EnableBatchProcessing
 @Import({ BatchTestSpringConfiguration.class })
+@Configuration("springBatchAssistantSpringConfiguration")
+@PropertySource("classpath:/spring_batch_assistant.property")
 @ComponentScan(basePackages = { "idv.hsiehpinghan.springbatchassistant" })
 public class SpringConfiguration extends DefaultBatchConfigurer {
-    @Autowired
-    private ResourceLoader resourceLoader;
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private ResourceLoader resourceLoader;
+	@Autowired
+	private DataSource dataSource;
 
-    @PostConstruct
-    protected void init() {
-        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(resourceLoader.getResource("classpath:/org/springframework/batch/core/schema-postgresql.sql"));
-        populator.setContinueOnError(true);
-        DatabasePopulatorUtils.execute(populator, dataSource);
-    }
+	@PostConstruct
+	protected void postConstruct() throws ScriptException, SQLException {
+		Resource resource = resourceLoader
+				.getResource("classpath:/org/springframework/batch/core/schema-postgresql.sql");
+		ScriptUtils.executeSqlScript(dataSource.getConnection(), resource);
+	}
+
+	@Bean
+	public DataSource dataSource(Environment environment)
+			throws PropertyVetoException {
+		String driverClass = environment
+				.getRequiredProperty("postgresql.driverClass");
+		String jdbcUrl = environment.getRequiredProperty("postgresql.jdbcUrl");
+		String user = environment.getRequiredProperty("postgresql.user");
+		String password = environment
+				.getRequiredProperty("postgresql.password");
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setDriverClass(driverClass);
+		comboPooledDataSource.setJdbcUrl(jdbcUrl);
+		comboPooledDataSource.setUser(user);
+		comboPooledDataSource.setPassword(password);
+		return comboPooledDataSource;
+	}
 
 }

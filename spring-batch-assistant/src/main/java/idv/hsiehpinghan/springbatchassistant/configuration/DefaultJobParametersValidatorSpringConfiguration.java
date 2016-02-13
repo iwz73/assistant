590552646ea8931a5451d117e3520e3a46282d3e
thank <thank@localhost>
2016-02-13 22:10:10 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/configuration/DefaultJobParametersValidatorSpringConfiguration.java
@@ -0,0 +1,44 @@
+package idv.hsiehpinghan.springbatchassistant.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultJobParametersValidatorSpringConfiguration {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job defaultJobParametersValidatorJob(
+			@Qualifier("defaultJobParametersValidator") JobParametersValidator jobParametersValidator,
+			@Qualifier("defaultJobParametersValidatorStep_0") Step defaultJobParametersValidatorStep_0) {
+		return jobBuilderFactory.get("defaultJobParametersValidatorJob")
+				.validator(jobParametersValidator)
+				.start(defaultJobParametersValidatorStep_0).build();
+	}
+
+	@Bean
+	protected Step defaultJobParametersValidatorStep_0(
+			@Qualifier("doNothingTasklet") Tasklet tasklet) {
+		return stepBuilderFactory.get("defaultJobParametersValidatorStep_0")
+				.tasklet(tasklet).build();
+	}
+
+	@Bean
+	public DefaultJobParametersValidator defaultJobParametersValidator() {
+		String[] requiredKeys = new String[] { "requiredKeys_0" };
+		String[] optionalKeys = new String[] { "optionalKeys_0" };
+		return new DefaultJobParametersValidator(requiredKeys, optionalKeys);
+	}
+}
---
+++ spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/DefaultJobParametersValidatorTest.java
@@ -0,0 +1,40 @@
+package idv.hsiehpinghan.springbatchassistant.test;
+
+import idv.hsiehpinghan.springbatchassistant.configuration.SpringConfiguration;
+
+import java.util.Date;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(classes = { SpringConfiguration.class })
+public class DefaultJobParametersValidatorTest extends
+		AbstractTestNGSpringContextTests {
+	@Autowired
+	private JobLauncher jobLauncher;
+	@Autowired
+	@Qualifier("defaultJobParametersValidatorJob")
+	private Job defaultJobParametersValidatorJob;
+
+	@Test(expectedExceptions = { JobParametersInvalidException.class })
+	public void withoutRequiredKeys() throws Exception {
+		JobParametersBuilder builder = new JobParametersBuilder();
+		jobLauncher.run(defaultJobParametersValidatorJob,
+				builder.toJobParameters());
+	}
+
+	@Test
+	public void withRequiredKeys() throws Exception {
+		JobParametersBuilder builder = new JobParametersBuilder();
+		builder.addDate("requiredKeys_0", new Date());
+		jobLauncher.run(defaultJobParametersValidatorJob,
+				builder.toJobParameters());
+	}
+}

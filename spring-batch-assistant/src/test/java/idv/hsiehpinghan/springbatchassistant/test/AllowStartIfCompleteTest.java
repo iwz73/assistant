a01aaa2c649ac74d769b27222b9f75aecfdeefad
thank <thank@localhost>
2016-02-13 22:56:24 +0800

    backup.

---
+++ spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/AllowStartIfCompleteTest.java
@@ -0,0 +1,36 @@
+package idv.hsiehpinghan.springbatchassistant.test;
+
+import idv.hsiehpinghan.springbatchassistant.configuration.SpringConfiguration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(classes = { SpringConfiguration.class })
+public class AllowStartIfCompleteTest extends AbstractTestNGSpringContextTests {
+	@Autowired
+	private JobLauncher jobLauncher;
+	@Autowired
+	@Qualifier("allowStartIfCompleteJob")
+	private Job allowStartIfCompleteJob;
+
+	@Test
+	public void test() throws Exception {
+		JobParametersBuilder builder = new JobParametersBuilder();
+		JobExecution jobExecution = null;
+		jobExecution = jobLauncher.run(allowStartIfCompleteJob,
+				builder.toJobParameters());
+		Assert.assertEquals(jobExecution.getStepExecutions().size(), 1);
+		jobExecution = jobLauncher.run(allowStartIfCompleteJob,
+				builder.toJobParameters());
+		Assert.assertEquals(jobExecution.getStepExecutions().size(), 1);
+	}
+
+}

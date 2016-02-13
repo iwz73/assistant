65aba2d6863d07b41270337879f505f6ee7c7ad3
thank <thank@localhost>
2016-02-13 23:41:17 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/configuration/SkipSpringConfiguration.java
@@ -0,0 +1,39 @@
+package idv.hsiehpinghan.springbatchassistant.configuration;
+
+import java.io.IOException;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SkipSpringConfiguration {
+	private static final Class<? extends Throwable> SKIP_CLASS = IOException.class;
+	private static final int SKIP_LIMIT = 3;
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job skipJob(@Qualifier("skipStep_0") Step skipStep_0) {
+		return jobBuilderFactory.get("skipJob").start(skipStep_0).build();
+	}
+
+	@Bean
+	protected Step skipStep_0(
+			@Qualifier("skipReader") ItemReader<String> reader,
+			@Qualifier("skipWriter") ItemWriter<Integer> writer) {
+		return stepBuilderFactory.get("skipStep_0").<String, Integer> chunk(3)
+				.faultTolerant().skip(SKIP_CLASS).skipLimit(SKIP_LIMIT)
+				.reader(reader).writer(writer).build();
+	}
+
+}
---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/reader/SkipReader.java
@@ -0,0 +1,32 @@
+package idv.hsiehpinghan.springbatchassistant.reader;
+
+import java.io.IOException;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SkipReader implements ItemReader<String> {
+	private final static String[] STRINGS = new String[] { "0", "1", "2", "3",
+			"4", "5", "6", "7", "8", "9" };
+	private static int index = 0;
+
+	@Override
+	public String read() throws Exception, UnexpectedInputException,
+			ParseException, NonTransientResourceException {
+		if (index >= STRINGS.length) {
+			return null;
+		}
+		String result = STRINGS[index];
+		index++;
+		if (index % 2 == 0) {
+			String msg = "SkipReader exception test !!!";
+			System.err.println(msg);
+			throw new IOException(msg);
+		}
+		return result;
+	}
+}

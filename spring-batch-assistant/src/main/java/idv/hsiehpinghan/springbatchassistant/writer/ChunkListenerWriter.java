3f6a714fa239c84496420c9bd5c961bc2574449b
thank <thank@localhost>
2016-02-14 12:08:50 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/writer/ChunkListenerWriter.java
@@ -0,0 +1,16 @@
+package idv.hsiehpinghan.springbatchassistant.writer;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChunkListenerWriter implements ItemWriter<Integer> {
+
+	@Override
+	public void write(List<? extends Integer> datas) throws Exception {
+		System.err.println("ChunkListenerWriter write : " + datas);
+	}
+
+}

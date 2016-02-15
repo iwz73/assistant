c94a45b9b90dae8aeac1573f6338573edca2b8e4
thank <thank@localhost>
2016-02-15 18:49:11 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/tasklet/SplitFlowTasklet.java
@@ -0,0 +1,20 @@
+package idv.hsiehpinghan.springbatchassistant.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SplitFlowTasklet implements Tasklet {
+	@Override
+	public RepeatStatus execute(StepContribution stepContribution,
+			ChunkContext chunkContext) throws Exception {
+		System.err.println("Before SplitFlowTasklet.");
+		Thread.sleep(5000);
+		System.err.println("After SplitFlowTasklet.");
+		return RepeatStatus.FINISHED;
+	}
+
+}
---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/writer/SplitFlowWriter_0.java
@@ -0,0 +1,17 @@
+package idv.hsiehpinghan.springbatchassistant.writer;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SplitFlowWriter_0 implements ItemWriter<Integer> {
+
+	@Override
+	public void write(List<? extends Integer> datas) throws Exception {
+		System.err.println("SplitFlowWriter_0 write : " + datas);
+		Thread.sleep(1000);
+	}
+
+}

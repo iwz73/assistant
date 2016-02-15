5838f06f6ea13b4fc77dca3d80863952e9621d32
thank <thank@localhost>
2016-02-15 16:04:06 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/configuration/StopAndRestartSpringConfiguration.java
@@ -0,0 +1,47 @@
+package idv.hsiehpinghan.springbatchassistant.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StopAndRestartSpringConfiguration {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job stopAndRestartJob(
+			@Qualifier("stopAndRestartStep_0") Step stopAndRestartStep_0,
+			@Qualifier("stopAndRestartStep_1") Step stopAndRestartStep_1) {
+		return jobBuilderFactory.get("stopAndRestartJob")
+				.start(stopAndRestartStep_0).on("COMPLETED")
+				.stopAndRestart(stopAndRestartStep_1).build().build();
+	}
+
+	@Bean
+	protected Step stopAndRestartStep_0(
+			@Qualifier("stopAndRestartReader_0") ItemReader<String> reader,
+			@Qualifier("stopAndRestartWriter_0") ItemWriter<Integer> writer) {
+		return stepBuilderFactory.get("stopAndRestartStep_0")
+				.<String, Integer> chunk(3).reader(reader).writer(writer)
+				.build();
+	}
+
+	@Bean
+	protected Step stopAndRestartStep_1(
+			@Qualifier("stopAndRestartReader_1") ItemReader<String> reader,
+			@Qualifier("stopAndRestartWriter_1") ItemWriter<Integer> writer) {
+		return stepBuilderFactory.get("stopAndRestartStep_1")
+				.<String, Integer> chunk(3).reader(reader).writer(writer)
+				.build();
+	}
+}

1edb2656f6a96993f6effeff8cd6dceacfa2dc65
thank <thank@localhost>
2016-02-15 15:33:11 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/configuration/FailSpringConfiguration.java
@@ -0,0 +1,35 @@
+package idv.hsiehpinghan.springbatchassistant.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FailSpringConfiguration {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job failJob(@Qualifier("failStep_0") Step failStep_0) {
+		return jobBuilderFactory.get("failJob").start(failStep_0)
+				.on("COMPLETED").fail().build().build();
+	}
+
+	@Bean
+	protected Step failStep_0(
+			@Qualifier("failReader") ItemReader<String> reader,
+			@Qualifier("failWriter") ItemWriter<Integer> writer) {
+		return stepBuilderFactory.get("failStep_0").<String, Integer> chunk(3)
+				.reader(reader).writer(writer).build();
+	}
+
+}

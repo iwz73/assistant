e91452547015697fdf8ad1578a905c09b1b1b353
thank <thank@localhost>
2016-02-20 18:42:27 +0800

    backup.

--- spring-data-hadoop-assistant/src/test/java/idv/hsiehpinghan/springdatahadoopassistant/assistant/HbaseAssistantTest.java
+++ spring-data-hadoop-assistant/src/test/java/idv/hsiehpinghan/springdatahadoopassistant/assistant/HbaseAssistantTest.java
@@ -1,12 +1,13 @@
 package idv.hsiehpinghan.springdatahadoopassistant.assistant;
 
 import idv.hsiehpinghan.springdatahadoopassistant.entity.Webpage;
+import idv.hsiehpinghan.springdatahadoopassistant.filter.TestFilter;
 import idv.hsiehpinghan.springdatahadoopassistant.suit.TestngSuitSetting;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
 import java.util.Collection;
+import java.util.Date;
 
 import org.apache.hadoop.hbase.filter.BinaryComparator;
 import org.apache.hadoop.hbase.filter.CompareFilter;
@@ -15,6 +16,8 @@ import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.PageFilter;
 import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.filter.SingleColumnValueExcludeFilter;
+import org.apache.hadoop.hbase.filter.SkipFilter;
+import org.apache.hadoop.hbase.filter.ValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.context.ApplicationContext;
 import org.testng.Assert;
@@ -35,30 +38,40 @@ public class HbaseAssistantTest {
 
 	@Test
 	public void scan() throws Exception {
-//		TestRowFilter();
+		// TestRowFilter();
 		// TestPageFilter();
-//		 TestSingleColumnValueExcludeFilter();
-		TestFilterList();
+		// TestSingleColumnValueExcludeFilter();
+		 TestFilterList();
+//		TestTestFilter();
 	}
 
-	
+	private void TestTestFilter() throws Exception {
+		Filter filter = new TestFilter();
+		Collection<Webpage> entities = assistant.scan(TABLE_NAME, filter);
+		for (Webpage entity : entities) {
+			System.err.println(entity.getRowKey() + " / "
+					+ entity.getF().getPrevFetchTime());
+		}
+		Assert.assertTrue(0 < entities.size());
+	}
+
 	private void TestFilterList() throws Exception {
 		Filter rowFilter = generateRowFilter();
-		Filter singleColumnValueExcludeFilter = generateSingleColumnValueExcludeFilter();
-		FilterList filterList = new FilterList(rowFilter);
+		Filter skipFilter = generateSkipFilter();
+		FilterList filterList = new FilterList(rowFilter, skipFilter);
 		Collection<Webpage> entities = assistant.scan(TABLE_NAME, filterList);
 		for (Webpage entity : entities) {
 			System.err.println(entity);
 		}
 		Assert.assertTrue(0 < entities.size());
 	}
-	
-	
+
 	private void TestSingleColumnValueExcludeFilter() throws Exception {
 		Filter filter = generateSingleColumnValueExcludeFilter();
 		Collection<Webpage> entities = assistant.scan(TABLE_NAME, filter);
 		for (Webpage entity : entities) {
-			System.err.println(entity.getRowKey() + " / " + entity.getF().getPrevFetchTime());
+			System.err.println(entity.getRowKey() + " / "
+					+ entity.getF().getPrevFetchTime());
 		}
 		Assert.assertTrue(0 < entities.size());
 	}
@@ -81,17 +94,35 @@ public class HbaseAssistantTest {
 		}
 		Assert.assertTrue(PAGE_SIZE < entities.size());
 	}
+
 	private Filter generateRowFilter() {
 		return new RowFilter(CompareFilter.CompareOp.EQUAL,
 				new BinaryComparator(
 						Bytes.toBytes("tw.com.ipeen.www:http/comment/148690")));
 	}
-	
+
 	private Filter generateSingleColumnValueExcludeFilter() {
 		Date targetDate = Date.from(LocalDate.of(2016, 2, 11).atStartOfDay()
 				.atZone(ZoneId.systemDefault()).toInstant());
 		return new SingleColumnValueExcludeFilter(Bytes.toBytes("f"),
 				Bytes.toBytes("pts"), CompareFilter.CompareOp.GREATER,
 				Bytes.toBytes(targetDate.getTime()));
+	}
+	
+	private Filter generateSkipFilter() {
+		Date targetDate_0 = Date.from(LocalDate.of(2016, 2, 11).atStartOfDay()
+				.atZone(ZoneId.systemDefault()).toInstant());
+		Filter filter_0 = new SingleColumnValueExcludeFilter(Bytes.toBytes("f"),
+				Bytes.toBytes("pts"), CompareFilter.CompareOp.GREATER,
+				Bytes.toBytes(targetDate_0.getTime()));
+		
+		Date targetDate_1 = Date.from(LocalDate.of(2016, 2, 21).atStartOfDay()
+				.atZone(ZoneId.systemDefault()).toInstant());
+		Filter filter_1 = new SingleColumnValueExcludeFilter(Bytes.toBytes("f"),
+				Bytes.toBytes("pts"), CompareFilter.CompareOp.LESS_OR_EQUAL,
+				Bytes.toBytes(targetDate_1.getTime()));
+		
+		FilterList filterList = new FilterList(filter_0, filter_1);
+		return new SkipFilter(filterList);
 	}
 }

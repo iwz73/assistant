06ad35a35f05f5f3822ec992b499edd380205574
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-06-11 01:38:47 +0800

    backup.

--- mapreduce-assistant-2/src/test/java/idv/hsiehpinghan/mapreduceassistant2/test/WordCount2.java
+++ mapreduce-assistant-2/src/test/java/idv/hsiehpinghan/mapreduceassistant2/test/WordCount2.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
 
@@ -20,19 +18,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.util.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WordCount2 {
 	public boolean count(Configuration conf, Path inputPath, Path outputPath, Path patternPath) throws Exception {
-//		GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
-//		String[] remainingArgs = optionParser.getRemainingArgs();
-//		if (!(remainingArgs.length != 2 || remainingArgs.length != 4)) {
-//			System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
-//			System.exit(2);
-//		}
 		Job job = Job.getInstance(conf, "word count 2");
 		job.setJarByClass(WordCount2.class);
 		job.setMapperClass(TokenizerMapper.class);
@@ -40,80 +30,45 @@ public class WordCount2 {
 		job.setReducerClass(IntSumReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-
-//		List<String> otherArgs = new ArrayList<String>();
-//		for (int i = 0; i < remainingArgs.length; ++i) {
-//			if ("-skip".equals(remainingArgs[i])) {
-//				job.addCacheFile(new Path(remainingArgs[++i]).toUri());
-//				job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
-//			} else {
-//				otherArgs.add(remainingArgs[i]);
-//			}
-//		}
-		
-//		job.addCacheFile(new Path(remainingArgs[++i]).toUri());
-//		job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
-		
 		job.addCacheFile(patternPath.toUri());
 		FileInputFormat.addInputPath(job, inputPath);
 		FileOutputFormat.setOutputPath(job, outputPath);
-
-		
-//		System.err.println(otherArgs.get(0));
-//		System.err.println(otherArgs.get(1));
-		
-		
 		return job.waitForCompletion(true);
 	}
-	
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 		static enum CountersEnum {
 			INPUT_WORDS
 		}
 
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
-
-		private boolean caseSensitive;
 		private Set<String> patternsToSkip = new HashSet<String>();
-
 		private Configuration conf;
 		private BufferedReader fis;
 
 		@Override
 		public void setup(Context context) throws IOException, InterruptedException {
 			conf = context.getConfiguration();
-			caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
-			if (conf.getBoolean("wordcount.skip.patterns", true)) {
-				URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
-				
-				System.err.println(patternsURIs);
-				
-				for (URI patternsURI : patternsURIs) {
-					Path patternsPath = new Path(patternsURI.getPath());
-					String patternsFileName = patternsPath.getName().toString();
-					parseSkipFile(patternsFileName);
-				}
+			URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
+			for (URI patternsURI : patternsURIs) {
+				Path patternsPath = new Path(patternsURI.getPath());
+				String patternsFileName = patternsPath.getName().toString();
+				parseSkipFile(patternsFileName);
 			}
 		}
 
-		private void parseSkipFile(String fileName) {
-			try {
-				fis = new BufferedReader(new FileReader(fileName));
-				String pattern = null;
-				while ((pattern = fis.readLine()) != null) {
-					patternsToSkip.add(pattern);
-				}
-			} catch (IOException ioe) {
-				System.err.println(
-						"Caught exception while parsing the cached file '" + StringUtils.stringifyException(ioe));
+		private void parseSkipFile(String fileName) throws IOException {
+			fis = new BufferedReader(new FileReader(fileName));
+			String pattern = null;
+			while ((pattern = fis.readLine()) != null) {
+				patternsToSkip.add(pattern);
 			}
 		}
 
 		@Override
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			String line = (caseSensitive) ? value.toString() : value.toString().toLowerCase();
+			String line = value.toString();
 			for (String pattern : patternsToSkip) {
 				line = line.replaceAll(pattern, "");
 			}

6f6eb851134d61632f3f71e3a5cc5dd1678acf2f
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-07-02 10:07:14 +0800

    backup.

---
+++ jersey-assistant/src/main/java/idv/hsiehpinghan/jerseyassistant/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package idv.hsiehpinghan.jerseyassistant.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "idv.hsiehpinghan.springmvcassistant" })
+public class SpringConfiguration {
+
+}

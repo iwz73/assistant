f0bc828ac99bca97857278dda2e99ae3ddb01688
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-08-29 00:22:15 +0800

    backup.

---
+++ solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/EDisMaxQueryTest.java
@@ -0,0 +1,61 @@
+package idv.hsiehpinghan.solrassistant.assistant;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import idv.hsiehpinghan.solrassistant.configuration.SpringConfiguration;
+import idv.hsiehpinghan.solrassistant.document.DefaultDocument;
+
+@ContextConfiguration(classes = { SpringConfiguration.class })
+public class EDisMaxQueryTest extends AbstractTestNGSpringContextTests {
+
+	@Autowired
+	private SolrAssistant solrAssistant;
+
+	@Test
+	public void fieldedTermSearchTest() throws Exception {
+		queryFieldsTest();
+		boostsQueryFieldsTest();
+	}
+	
+	@Test
+	public void boostQueryTest() throws Exception {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("q={!edismax bq=\"manufacturedate_dt:[2005-01-01T00:00:00Z TO 2006-01-01T00:00:00Z]\"}\"electronics\"");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.get(0).getFieldValue("id"), "F8V7067-APL-KIT");
+	}
+
+	private void queryFieldsTest() throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("q={!edismax qf=\"name manu\"}\"with\" OR \"here\"");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 3);
+	}
+	
+	private void boostsQueryFieldsTest() throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("q={!edismax qf=\"name cat includes^10 features\"}\"card\"");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.get(0).getFieldValue("id"), "9885A004");
+	}
+	
+}
--- solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/LuceneQueryTest.java
+++ solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/LuceneQueryTest.java
@@ -1,17 +1,9 @@
 package idv.hsiehpinghan.solrassistant.assistant;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.apache.commons.lang3.time.DateUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
-import org.apache.solr.client.solrj.response.UpdateResponse;
-import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.SolrInputDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -19,7 +11,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import idv.hsiehpinghan.solrassistant.configuration.SpringConfiguration;
-import idv.hsiehpinghan.solrassistant.document.DefaultDocument;
 
 @ContextConfiguration(classes = { SpringConfiguration.class })
 public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
@@ -32,40 +23,40 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 		termSearchTest();
 		multipleTermSearchTest();
 		appearTogetherTermSearchTest();
-		
+
 	}
-	
+
 	@Test
 	public void requiredTermTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
-//		solrQuery.setQuery("+name:\"ipod\" +id:\"F8V7067-APL-KIT\"");
-//		solrQuery.setQuery("name:\"ipod\" && id:\"F8V7067-APL-KIT\"");
+		// solrQuery.setQuery("+name:\"ipod\" +id:\"F8V7067-APL-KIT\"");
+		// solrQuery.setQuery("name:\"ipod\" && id:\"F8V7067-APL-KIT\"");
 		solrQuery.setQuery("name:\"ipod\" AND id:\"F8V7067-APL-KIT\"");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
-	
+
 	@Test
 	public void excludingTermsTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
-//		solrQuery.setQuery("-name:\"ipod\" -id:\"F8V7067-APL-KIT\"");
+		// solrQuery.setQuery("-name:\"ipod\" -id:\"F8V7067-APL-KIT\"");
 		solrQuery.setQuery("name:\"ipod\" AND NOT id:\"F8V7067-APL-KIT\"");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 2);
 	}
-	
+
 	@Test
 	public void optionalTermTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
-//		solrQuery.setQuery("name:\"ipod\" || id:\"F8V7067-APL-KIT\"");
+		// solrQuery.setQuery("name:\"ipod\" || id:\"F8V7067-APL-KIT\"");
 		solrQuery.setQuery("name:\"ipod\" OR id:\"F8V7067-APL-KIT\"");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 3);
 	}
-	
+
 	@Test
 	public void groupedExpressionTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
@@ -74,7 +65,7 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
-	
+
 	@Test
 	public void termProximityTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
@@ -83,7 +74,7 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
-	
+
 	@Test
 	public void characterProximityTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
@@ -92,17 +83,48 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 2);
 	}
-	
+
 	@Test
-	public void rangeSearchesTest() throws Exception {
+	public void rangeSearchTest() throws Exception {
 		numberRangeSearchesTest();
 		dateRangeSearchesTest();
 		stringRangeSearchesTest();
 	}
-	
+
+	@Test
+	public void wildcardSearchTest() throws Exception {
+		asteriskWildcardSearchTest();
+		questionMarkWildcardSearchTest();
+	}
+
+	@Test
+	public void boostingExpressionsTest() throws Exception {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("name:\"ipod\"^10 OR manu:\"Samsung\"^0.5");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 4);
+	}
+
+	private void asteriskWildcardSearchTest() throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("name:c*");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 9);
+	}
+
+	private void questionMarkWildcardSearchTest() throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("name:m?ni");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 1);
+	}
+
 	private void stringRangeSearchesTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("manu:[\"Belkin\" TO \"C\"]");
+		solrQuery.setQuery("manu:[\"Belkin\" TO \"C\"}");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 2);
@@ -110,20 +132,20 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 
 	private void dateRangeSearchesTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("manufacturedate_dt:[2005-08-01T16:30:25Z TO NOW-1DAY]");
+		solrQuery.setQuery("manufacturedate_dt:[2005-08-01T16:30:25Z TO NOW-1DAY}");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 10);
 	}
-	
+
 	private void numberRangeSearchesTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("weight:[2 TO 4]");
+		solrQuery.setQuery("weight:[2 TO 4}");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
-		Assert.assertEquals(solrDocumentList.size(), 2);
+		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
-	
+
 	private void termSearchTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
 		solrQuery.setQuery("name:\"ipod\" AND id:\"F8V7067-APL-KIT\"");
@@ -139,7 +161,7 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 4);
 	}
-	
+
 	private void appearTogetherTermSearchTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
 		solrQuery.setQuery("name:\"for iPod\"");
@@ -147,5 +169,5 @@ public class LuceneQueryTest extends AbstractTestNGSpringContextTests {
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
-	
+
 }

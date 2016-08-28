47a72c4f20a4c48f083b349a513e9666917ee85e
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-08-29 07:55:03 +0800

    backup.

--- solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/EDisMaxQueryTest.java
+++ solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/EDisMaxQueryTest.java
@@ -36,15 +36,48 @@ public class EDisMaxQueryTest extends AbstractTestNGSpringContextTests {
 	@Test
 	public void boostQueryTest() throws Exception {
 		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("q={!edismax bq=\"manufacturedate_dt:[2005-01-01T00:00:00Z TO 2006-01-01T00:00:00Z]\"}\"electronics\"");
+		solrQuery.setQuery("{!edismax bq=\"manufacturedate_dt:[2005-01-01T00:00:00Z TO 2006-01-01T00:00:00Z]\"}\"electronics\"");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.get(0).getFieldValue("id"), "F8V7067-APL-KIT");
 	}
 
+	@Test
+	public void fieldAliasingTest() throws Exception {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("{!edismax f.alias_0.qf=\"name features^10\"}alias_0:\"ipod\"");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 3);
+	}
+	
+	@Test
+	public void userAccessibleFieldTest() throws Exception {
+		restrictAvailableFieldsTest();
+		disableFieldsTest();
+	}
+		
+	
+	
+	private void restrictAvailableFieldsTest() throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("{!edismax uf=\"cat manu\"}name:ipod");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 0);
+	}
+	
+	private void disableFieldsTest() throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("{!edismax uf=\"-name -cat\"}name:ipod");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 0);
+	}
+	
 	private void queryFieldsTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("q={!edismax qf=\"name manu\"}\"with\" OR \"here\"");
+		solrQuery.setQuery("{!edismax qf=\"name manu\"}\"with\" OR \"here\"");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.size(), 3);
@@ -52,7 +85,7 @@ public class EDisMaxQueryTest extends AbstractTestNGSpringContextTests {
 	
 	private void boostsQueryFieldsTest() throws SolrServerException {
 		SolrQuery solrQuery = new SolrQuery();
-		solrQuery.setQuery("q={!edismax qf=\"name cat includes^10 features\"}\"card\"");
+		solrQuery.setQuery("{!edismax qf=\"name cat includes^10 features\"}\"card\"");
 		QueryResponse response = solrAssistant.query(solrQuery);
 		SolrDocumentList solrDocumentList = response.getResults();
 		Assert.assertEquals(solrDocumentList.get(0).getFieldValue("id"), "9885A004");

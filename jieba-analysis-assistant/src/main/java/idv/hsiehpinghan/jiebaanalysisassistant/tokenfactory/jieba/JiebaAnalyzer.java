a5a91bba6b75732924974d3dad7e0f874588b736
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-09-10 11:31:53 +0800

    backup.

---
+++ jieba-analysis-assistant/src/main/java/idv/hsiehpinghan/jiebaanalysisassistant/tokenfactory/jieba/JiebaAnalyzer.java
@@ -0,0 +1,34 @@
+package idv.hsiehpinghan.jiebaanalysisassistant.tokenfactory.jieba;
+
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+
+import com.huaban.analysis.jieba.JiebaSegmenter.SegMode;
+
+/**
+ * copy from https://github.com/sing1ee/jieba-solr/ for test.
+ * 
+ * @author thank
+ *
+ */
+public class JiebaAnalyzer extends Analyzer {
+	private String segMode;
+
+	public JiebaAnalyzer() {
+		this(SegMode.SEARCH.name());
+	}
+
+	public JiebaAnalyzer(String segMode) {
+		this.segMode = segMode;
+	}
+
+	public JiebaAnalyzer(ReuseStrategy reuseStrategy) {
+		super(reuseStrategy);
+	}
+
+	@Override
+	protected TokenStreamComponents createComponents(String field, Reader in) {
+		return new TokenStreamComponents(new JiebaTokenizer(in, this.segMode));
+	}
+}

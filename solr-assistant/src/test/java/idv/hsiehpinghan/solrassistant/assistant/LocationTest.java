0c46489b4421613b2c5e6a240b53742f42b98389
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-09-25 00:02:09 +0800

    backup.

--- solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/LocationTest.java
+++ solr-assistant/src/test/java/idv/hsiehpinghan/solrassistant/assistant/LocationTest.java
@@ -21,6 +21,7 @@ public class LocationTest extends AbstractTestNGSpringContextTests {
 	private static final String SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_POINT = "-90.57341 43.17614";
 	private static final String SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_RECTANGLE = "-74.093 41.042 -69.347 44.558";
 	private static final String SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_CIRCLE = "Circle(37.775,-122.419 d=20)";
+	private static final String SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_POLYGON = "POLYGON((-10 30, -40 40, -10 -20, 40 20, 0 0, -10 30))";
 	private SolrInputDocument solrInputDocument = generateSolrInputDocument();
 	private SolrQuery solrQuery = generateSolrQuery();
 	@Autowired
@@ -70,14 +71,25 @@ public class LocationTest extends AbstractTestNGSpringContextTests {
 		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
 
-	@AfterClass
-	private void afterClass() throws Exception {
-		String id = String.valueOf(solrInputDocument.getFieldValue("id"));
-		UpdateResponse deleteResponse = solrAssistant.deleteById(id);
-		Assert.assertEquals(deleteResponse.getStatus(), 0);
-		QueryResponse queryResponse = solrAssistant.query(solrQuery);
-		Assert.assertEquals(queryResponse.getResults().size(), 0);
+	@Test
+	public void polygonTest() throws Exception {
+		SolrQuery solrQuery = new SolrQuery();
+		solrQuery.setQuery("*:*");
+		solrQuery.addFilterQuery("id :" + solrInputDocument.getFieldValue("id"));
+		solrQuery.addFilterQuery("{!geofilt sfield=spatial_recursive_prefix_tree_field_type_polygon pt=20,-20 d=1}");
+		QueryResponse response = solrAssistant.query(solrQuery);
+		SolrDocumentList solrDocumentList = response.getResults();
+		Assert.assertEquals(solrDocumentList.size(), 1);
 	}
+
+//	@AfterClass
+//	private void afterClass() throws Exception {
+//		String id = String.valueOf(solrInputDocument.getFieldValue("id"));
+//		UpdateResponse deleteResponse = solrAssistant.deleteById(id);
+//		Assert.assertEquals(deleteResponse.getStatus(), 0);
+//		QueryResponse queryResponse = solrAssistant.query(solrQuery);
+//		Assert.assertEquals(queryResponse.getResults().size(), 0);
+//	}
 
 	private SolrInputDocument generateSolrInputDocument() {
 		SolrInputDocument doc = new SolrInputDocument();
@@ -87,6 +99,8 @@ public class LocationTest extends AbstractTestNGSpringContextTests {
 				SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_RECTANGLE);
 		doc.setField("spatial_recursive_prefix_tree_field_type_circle",
 				SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_CIRCLE);
+		doc.setField("spatial_recursive_prefix_tree_field_type_polygon",
+				SPATIAL_RECURSIVE_PREFIX_TREE_FIELD_TYPE_POLYGON);
 		return doc;
 	}
 

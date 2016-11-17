54266a284c96565ebac6ad018a2941c6ff6063f3
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-17 17:02:31 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/writer/LinearFlowWriter.java
@@ -0,0 +1,14 @@
+package idv.hsiehpinghan.springbatchassistant.writer;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+
+public class LinearFlowWriter implements ItemWriter<Integer> {
+
+	@Override
+	public void write(List<? extends Integer> items) throws Exception {
+		System.err.println("LinearFlow write items(" + items + ")");
+	}
+
+}
--- spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/LinearFlowTest.java
+++ spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/LinearFlowTest.java
@@ -13,36 +13,16 @@ import org.testng.annotations.Test;
 
 @ContextConfiguration(locations = { "classpath:/batch/LinearFlow.xml" })
 public class LinearFlowTest extends AbstractTestNGSpringContextTests {
-	private final String FILE_RESOURCE = "fileResource";
-	private final String TARGET_DIRECTORY = "targetDirectory";
-	private final String TARGET_FILE = "targetFile";
-
 	@Autowired
 	private JobLauncher jobLauncher;
 	@Autowired
-	@Qualifier("LinearFlowJob")
+	@Qualifier("linearFlowJob")
 	private Job job;
 
 	@Test
 	public void test() throws Exception {
 		JobParametersBuilder builder = new JobParametersBuilder();
-		builder.addString("fileResource", FILE_RESOURCE + "tttjj");
-		builder.addString("targetDirectory", TARGET_DIRECTORY);
-		builder.addString("targetFile", TARGET_FILE);
 		JobParameters jobParameters = builder.toJobParameters();
-
 		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
-		// Assert.assertEquals(jobExecution.getStepExecutions().size(), 1);
-		// jobExecution = jobLauncher.run(job,
-		// builder.toJobParameters());
-		// Assert.assertEquals(jobExecution.getStepExecutions().size(), 1);
-
-		// jobLauncher.run(job, new JobParametersBuilder()
-		// .addString("inputResource", "classpath:/input/products.zip")
-		// .addString("targetDirectory", "./target/importproductsbatch/")
-		// .addString("targetFile","products.txt")
-		// .addLong("timestamp", System.currentTimeMillis())
-		// .toJobParameters()
-		// );
 	}
 }

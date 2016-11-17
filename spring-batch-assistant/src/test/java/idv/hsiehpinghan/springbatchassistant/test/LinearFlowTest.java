03e08ef6f7cca5c5c18acf8aa3a835e37b283bbd
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-17 18:12:02 +0800

    backup.

--- spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/LinearFlowTest.java
+++ spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/LinearFlowTest.java
@@ -1,5 +1,6 @@
 package idv.hsiehpinghan.springbatchassistant.test;
 
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(locations = { "classpath:/batch/LinearFlow.xml" })
@@ -21,8 +23,9 @@ public class LinearFlowTest extends AbstractTestNGSpringContextTests {
 
 	@Test
 	public void test() throws Exception {
-		JobParametersBuilder builder = new JobParametersBuilder();
-		JobParameters jobParameters = builder.toJobParameters();
+		JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+		JobParameters jobParameters = jobParametersBuilder.toJobParameters();
 		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+		Assert.assertEquals(jobExecution.getExitStatus().getExitCode(), ExitStatus.COMPLETED.getExitCode());
 	}
 }

8a95031be43bc27a7bfe98ba19988a788f3e8f99
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-21 16:43:54 +0800

    backup.

---
+++ spring-batch-assistant/src/test/java/idv/hsiehpinghan/springbatchassistant/test/MultiResourceTest.java
@@ -0,0 +1,46 @@
+package idv.hsiehpinghan.springbatchassistant.test;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import idv.hsiehpinghan.springbatchassistant.configuration.SpringConfiguration;
+
+@ContextConfiguration(classes = { SpringConfiguration.class })
+public class MultiResourceTest extends AbstractTestNGSpringContextTests {
+	@Autowired
+	private JobLauncher jobLauncher;
+	@Autowired
+	@Qualifier("multiResourceJob")
+	private Job job;
+
+	@Test
+	public void test() throws Exception {
+		JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+		jobParametersBuilder.addString("encoding", "utf-8");
+		jobParametersBuilder.addLong("linesToSkip", 1L);
+		jobParametersBuilder.addString("strict", "true");
+		jobParametersBuilder.addString("inputResources",
+				"/home/thank/git/assistant/spring-batch-assistant/src/test/data/multiResource/multiResource_*");
+		jobParametersBuilder.addString("comments", "!,#");
+		jobParametersBuilder.addString("appendAllowed", "false");
+		jobParametersBuilder.addString("lineSeparator", System.lineSeparator());
+		jobParametersBuilder.addString("saveState", "true");
+		jobParametersBuilder.addString("shouldDeleteIfEmpty", "false");
+		jobParametersBuilder.addString("shouldDeleteIfExists", "true");
+		jobParametersBuilder.addString("transactional", "true");
+		jobParametersBuilder.addString("outputResource", "/tmp/multiResource");
+		JobParameters jobParameters = jobParametersBuilder.toJobParameters();
+		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+		Assert.assertEquals(jobExecution.getExitStatus().getExitCode(), ExitStatus.COMPLETED.getExitCode());
+	}
+}

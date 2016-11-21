990cb9bdf0ed90eb9f56c0534e046d74e260e7e7
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-21 15:42:13 +0800

    backup.

---
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/vo/FlatFileXmlVo.java
@@ -0,0 +1,43 @@
+package idv.hsiehpinghan.springbatchassistant.vo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "item")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class FlatFileXmlVo {
+	@XmlAttribute(name = "attributeValue")
+	private String attributeValue;
+	@XmlElement(name = "longValue")
+	private Long longValue;
+	@XmlElement(name = "stringValue")
+	private String stringValue;
+
+	public String getAttributeValue() {
+		return attributeValue;
+	}
+
+	public void setAttributeValue(String attributeValue) {
+		this.attributeValue = attributeValue;
+	}
+
+	public Long getLongValue() {
+		return longValue;
+	}
+
+	public void setLongValue(Long longValue) {
+		this.longValue = longValue;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+}

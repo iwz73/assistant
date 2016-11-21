cc253334cee6c6a3353510987a9ea8feb5b00655
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-21 13:53:06 +0800

    backup.

--- spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/mapper/FlatFileJsonLineMapper.java
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/mapper/FlatFileJsonLineMapper.java
@@ -6,6 +6,7 @@ import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.JsonLineMapper;
 
 import idv.hsiehpinghan.springbatchassistant.vo.FlatFileJsonVo;
+import idv.hsiehpinghan.springbatchassistant.vo.FlatFileJsonVo.SubMapFlatFileJsonVo;
 
 public class FlatFileJsonLineMapper implements LineMapper<FlatFileJsonVo> {
 	private JsonLineMapper delegate;
@@ -13,16 +14,32 @@ public class FlatFileJsonLineMapper implements LineMapper<FlatFileJsonVo> {
 	@Override
 	public FlatFileJsonVo mapLine(String line, int lineNumber) throws Exception {
 		Map<String, Object> map = delegate.mapLine(line, lineNumber);
-		FlatFileJsonVo vo = new FlatFileJsonVo();
-		vo.setLineNumber(lineNumber);
-		Long longValue = ((Integer)map.get("longValue")).longValue();
-		vo.setLongValue(longValue);
-		String stringValue = (String) map.get("stringValue");
-		vo.setStringValue(stringValue);
+		FlatFileJsonVo vo = processFlatFileJsonVo(map, lineNumber);
 		return vo;
 	}
 
 	public void setDelegate(JsonLineMapper delegate) {
 		this.delegate = delegate;
+	}
+
+	private FlatFileJsonVo processFlatFileJsonVo(Map<String, Object> map, int lineNumber) {
+		FlatFileJsonVo vo = new FlatFileJsonVo();
+		vo.setLineNumber(lineNumber);
+		Long longValue = ((Integer) map.get("longValue")).longValue();
+		vo.setLongValue(longValue);
+		String stringValue = (String) map.get("stringValue");
+		vo.setStringValue(stringValue);
+		SubMapFlatFileJsonVo subMapVo = processSubMapFlatFileJsonVo(map);
+		vo.setSubMapFlatFileJsonVo(subMapVo);
+		return vo;
+	}
+
+	private SubMapFlatFileJsonVo processSubMapFlatFileJsonVo(Map<String, Object> map) {
+		@SuppressWarnings("unchecked")
+		Map<String, Object> mapValue = (Map<String, Object>) map.get("mapValue");
+		Float floatValue = ((Double) mapValue.get("floatValue")).floatValue();
+		SubMapFlatFileJsonVo subMapVo = new SubMapFlatFileJsonVo();
+		subMapVo.setFloatValue(floatValue);
+		return subMapVo;
 	}
 }
--- spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/vo/FlatFileJsonVo.java
+++ spring-batch-assistant/src/main/java/idv/hsiehpinghan/springbatchassistant/vo/FlatFileJsonVo.java
@@ -4,6 +4,7 @@ public class FlatFileJsonVo {
 	private int lineNumber;
 	private Long longValue;
 	private String stringValue;
+	private SubMapFlatFileJsonVo subMapFlatFileJsonVo;
 
 	public int getLineNumber() {
 		return lineNumber;
@@ -27,6 +28,27 @@ public class FlatFileJsonVo {
 
 	public void setStringValue(String stringValue) {
 		this.stringValue = stringValue;
+	}
+
+	public SubMapFlatFileJsonVo getSubMapFlatFileJsonVo() {
+		return subMapFlatFileJsonVo;
+	}
+
+	public void setSubMapFlatFileJsonVo(SubMapFlatFileJsonVo subMapFlatFileJsonVo) {
+		this.subMapFlatFileJsonVo = subMapFlatFileJsonVo;
+	}
+
+	public static class SubMapFlatFileJsonVo {
+		private Float floatValue;
+
+		public Float getFloatValue() {
+			return floatValue;
+		}
+
+		public void setFloatValue(Float floatValue) {
+			this.floatValue = floatValue;
+		}
+
 	}
 
 }

ad0aefe1593c20ffa5e00ef88d319a2c30898b83
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-11-29 12:19:30 +0800

    backup.

--- mongodb-assistant/src/test/java/idv/hsiehpinghan/mongodbassistant/assistant/MongodbAssistantTest.java
+++ mongodb-assistant/src/test/java/idv/hsiehpinghan/mongodbassistant/assistant/MongodbAssistantTest.java
@@ -1,30 +1,24 @@
 package idv.hsiehpinghan.mongodbassistant.assistant;
 
-import idv.hsiehpinghan.mongodbassistant.entity.Person;
-import idv.hsiehpinghan.mongodbassistant.suit.TestngSuitSetting;
-
-import java.io.IOException;
-
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class MongodbAssistantTest {
-	private MongodbAssistant assistant;
+import idv.hsiehpinghan.mongodbassistant.configuration.SpringConfiguration;
+import idv.hsiehpinghan.mongodbassistant.entity.Person;
+
+@ContextConfiguration(classes = { SpringConfiguration.class })
+public class MongodbAssistantTest extends AbstractTestNGSpringContextTests {
 	private String databaseName = "testDatabase";
 	private String name = "name";
 	private int age = 38;
-
-	@BeforeClass
-	public void beforeClass() throws IOException {
-		ApplicationContext applicationContext = TestngSuitSetting
-				.getApplicationContext();
-		assistant = applicationContext.getBean(MongodbAssistant.class);
-	}
+	@Autowired
+	private MongodbAssistant assistant;
 
 	@Test
 	public void getMongoOperations() throws Exception {
@@ -35,7 +29,6 @@ public class MongodbAssistantTest {
 	private void testInsertByConstructor(MongoOperations operations) {
 		operations.dropCollection(Person.class.getSimpleName());
 		operations.insert(new Person(name, age));
-		Assert.assertNotNull(operations.findOne(new Query(Criteria
-				.where("name").is(name)), Person.class));
+		Assert.assertNotNull(operations.findOne(new Query(Criteria.where("name").is(name)), Person.class));
 	}
 }

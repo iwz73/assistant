edec132bd76ac470c96710b309a93df7eede568c
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2016-12-20 11:26:25 +0800

    backup.

---
+++ redis-assistant/src/main/java/idv/hsiehpinghan/redisassistant/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package idv.hsiehpinghan.redisassistant.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("redisAssistantSpringConfiguration")
+@ComponentScan(basePackages = { "idv.hsiehpinghan.redisassistant" })
+public class SpringConfiguration {
+
+}

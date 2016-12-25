b8a20331cbf5c79323185d3335a11318f4b9ac41
hsiehpinghan <hsiehpinghan@thank>
2016-12-25 10:46:50 +0800

    backup.

--- mongodb-assistant/src/test/java/idv/hsiehpinghan/mongodbassistant/utility/MongodbTestUtility.java
+++ mongodb-assistant/src/test/java/idv/hsiehpinghan/mongodbassistant/utility/MongodbTestUtility.java
@@ -18,21 +18,119 @@ import com.mongodb.client.MongoDatabase;
 public class MongodbTestUtility {
 	private static final String DATABASE_NAME = "mongodb_database";
 	private static final String COLLECTION_NAME = "mongodb_collection";
+	private static final String COLLECTION_JOIN_NAME = "mongodb_collection_join";
+	private static final String COLLECTION_TREE_NAME = "mongodb_collection_tree";
+	private static final String COLLECTION_GRAPH_NAME = "mongodb_collection_graph";
+	private static final String COLLECTION_TRAVELER_NAME = "mongodb_collection_traveler";
 
 	public static void init(ApplicationContext applicationContext) {
 		MongoClient mongoClient = applicationContext.getBean(MongoClient.class);
 		MongoDatabase db = mongoClient.getDatabase(DATABASE_NAME);
-		MongoCollection<Document> collection = db.getCollection(COLLECTION_NAME);
+		initCollection(db);
+		initCollectionJoin(db);
+		initCollectionTree(db);
+		initCollectionGraph(db);
+		initCollectionTraveler(db);
+	}
+
+	private static void initCollectionTraveler(MongoDatabase db) {
+		MongoCollection<Document> collection = db.getCollection(COLLECTION_TRAVELER_NAME);
+		collection.drop();
+		Document doc = new Document("_id", 10000);
+		doc.append("name", "traveler_0");
+		doc.append("hometown", "location_0");
+		collection.insertOne(doc);
+	}
+
+	private static void initCollectionGraph(MongoDatabase db) {
+		MongoCollection<Document> collection = db.getCollection(COLLECTION_GRAPH_NAME);
+		collection.drop();
+		List<Document> docs = new ArrayList<>(6);
+		Document doc0 = generateCollectionGraphDocument(1000, "location_0", Arrays.asList("location_1", "location_2"));
+		docs.add(doc0);
+		Document doc1 = generateCollectionGraphDocument(1001, "location_1", Arrays.asList("location_0", "location_3"));
+		docs.add(doc1);
+		Document doc2 = generateCollectionGraphDocument(1002, "location_2", Arrays.asList("location_0", "location_4"));
+		docs.add(doc2);
+		Document doc3 = generateCollectionGraphDocument(1003, "location_3", Arrays.asList("location_1", "location_4"));
+		docs.add(doc3);
+		Document doc4 = generateCollectionGraphDocument(1004, "location_4", Arrays.asList("location_3"));
+		docs.add(doc4);
+		collection.insertMany(docs);
+	}
+
+	private static void initCollectionTree(MongoDatabase db) {
+		MongoCollection<Document> collection = db.getCollection(COLLECTION_TREE_NAME);
+		collection.drop();
+		List<Document> docs = new ArrayList<>(6);
+		Document doc0 = generateCollectionTreeDocument(100, "name_0", null);
+		docs.add(doc0);
+		Document doc1 = generateCollectionTreeDocument(101, "name_1", "name_0");
+		docs.add(doc1);
+		Document doc2 = generateCollectionTreeDocument(102, "name_2_0", "name_1");
+		docs.add(doc2);
+		Document doc3 = generateCollectionTreeDocument(103, "name_2_1", "name_1");
+		docs.add(doc3);
+		Document doc4 = generateCollectionTreeDocument(104, "name_3_0", "name_2_0");
+		docs.add(doc4);
+		Document doc5 = generateCollectionTreeDocument(105, "name_3_1", "name_2_1");
+		docs.add(doc5);
+		collection.insertMany(docs);
+	}
+
+	private static void initCollectionJoin(MongoDatabase db) {
+		MongoCollection<Document> collection = db.getCollection(COLLECTION_JOIN_NAME);
 		collection.drop();
 		List<Document> docs = new ArrayList<>(2);
-		Document doc0 = generateDocument0();
+		Document doc0 = generateCollectionJoinDocument0();
 		docs.add(doc0);
-		Document doc1 = generateDocument1();
+		Document doc1 = generateCollectionJoinDocument1();
 		docs.add(doc1);
 		collection.insertMany(docs);
 	}
 
-	private static Document generateDocument0() {
+	private static void initCollection(MongoDatabase db) {
+		MongoCollection<Document> collection = db.getCollection(COLLECTION_NAME);
+		collection.drop();
+		List<Document> docs = new ArrayList<>(2);
+		Document doc0 = generateCollectionDocument0();
+		docs.add(doc0);
+		Document doc1 = generateCollectionDocument1();
+		docs.add(doc1);
+		collection.insertMany(docs);
+	}
+
+	private static Document generateCollectionGraphDocument(int id, String location, List<String> connectTo) {
+		Document doc = new Document("_id", id);
+		doc.append("location", location);
+		doc.append("connectTo", connectTo);
+		return doc;
+	}
+
+	private static Document generateCollectionTreeDocument(int id, String name, String reportTo) {
+		Document doc = new Document("_id", id);
+		doc.append("name", name);
+		if (reportTo != null) {
+			doc.append("reportTo", reportTo);
+		}
+		return doc;
+	}
+
+	private static Document generateCollectionJoinDocument0() {
+		Document doc = new Document("_id", 10);
+		doc.append("string_join", "string_0");
+		doc.append("array", Arrays.asList("繁體中文字串測試_10", "其他不相關字串_10", "text_10"));
+		return doc;
+	}
+
+	private static Document generateCollectionJoinDocument1() {
+		Document doc = new Document("_id", 11);
+		doc.append("string_join", "string_1");
+		doc.append("array", Arrays.asList("繁體中文字串測試_11", "其他不相關字串_11", "text_11"));
+		return doc;
+	}
+
+	private static Document generateCollectionDocument0() {
 		Document doc = new Document("_id", 0);
 		doc.append("double", 0.0);
 		doc.append("string", "string_0");
@@ -55,7 +153,7 @@ public class MongodbTestUtility {
 		return doc;
 	}
 
-	private static Document generateDocument1() {
+	private static Document generateCollectionDocument1() {
 		Document doc = new Document("_id", 1);
 		doc.append("double", 1.1);
 		doc.append("string", "string_1");

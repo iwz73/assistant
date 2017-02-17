189f3afef17be2e4f26e6abf3d634727d6ebcc3e
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-02-17 14:41:27 +0800

    backup.

---
+++ spring-security-assistant/src/main/java/idv/hsiehpinghan/springsecurityassistant/configuration/SpringConfiguration.java
@@ -0,0 +1,60 @@
+package idv.hsiehpinghan.springsecurityassistant.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@Import({ SpringSecurityConfiguration.class })
+@ComponentScan(basePackages = { "idv.hsiehpinghan.springsecurityassistant" })
+public class SpringConfiguration extends WebMvcConfigurationSupport {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/index.html").addResourceLocations("/");
+		registry.addResourceHandler("/image/**").addResourceLocations("/image/");
+		registry.addResourceHandler("/javascript/**").addResourceLocations("/javascript/");
+	}
+
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setContentNegotiationManager(manager);
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+		resolvers.add(urlBasedViewResolver());
+		resolver.setViewResolvers(resolvers);
+		return resolver;
+	}
+
+	private UrlBasedViewResolver urlBasedViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+}

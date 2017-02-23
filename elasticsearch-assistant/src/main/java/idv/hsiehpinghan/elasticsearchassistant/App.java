8905eacf02204349e235c856ac13a46387bf17a4
hsiehpinghan <hsiehpinghan@thank>
2017-02-23 22:14:13 +0800

    backup.

---
+++ elasticsearch-assistant/src/main/java/idv/hsiehpinghan/elasticsearchassistant/App.java
@@ -0,0 +1,13 @@
+package idv.hsiehpinghan.elasticsearchassistant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

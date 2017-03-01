9d869f4c1b336fd7caac684a59c98c9368418eec
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-03-01 15:39:44 +0800

    backup.

--- elasticsearch-assistant/src/main/java/idv/hsiehpinghan/elasticsearchassistant/App.java
+++ elasticsearch-assistant/src/main/java/idv/hsiehpinghan/elasticsearchassistant/App.java
@@ -1,13 +1,20 @@
 package idv.hsiehpinghan.elasticsearchassistant;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		Client client = TransportClient.builder().build()
+				.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+
+	}
 }

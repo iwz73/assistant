21b22bc68e03d316385d60f9c6e5c410656398e5
hsiehpinghan <hsiehpinghan@thank>
2017-03-29 21:56:55 +0800

    backup.

---
+++ xml-assistant/src/main/java/idv/hsiehpinghan/xmlassistant/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package idv.hsiehpinghan.xmlassistant.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("xmlAssistantSpringConfiguration")
+@ComponentScan(basePackages = { "idv.hsiehpinghan.xmlassistant" })
+public class SpringConfiguration {
+
+}
--- xml-assistant/src/test/java/idv/hsiehpinghan/xmlassistant/utility/DocumentUtilityTest.java
+++ xml-assistant/src/test/java/idv/hsiehpinghan/xmlassistant/utility/DocumentUtilityTest.java
@@ -1,24 +1,32 @@
 package idv.hsiehpinghan.xmlassistant.utility;
 
-import idv.hsiehpinghan.testutility.utility.SystemResourceUtility;
-
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-public class DocumentUtilityTest {
+import idv.hsiehpinghan.xmlassistant.configuration.SpringConfiguration;
+
+@ContextConfiguration(classes = { SpringConfiguration.class })
+public class DocumentUtilityTest extends AbstractTestNGSpringContextTests {
 	private Document document;
+	@Autowired
+	private ResourceLoader resourceLoader;
 	
 	@Test
 	public void parse() throws Exception {
-		File file = SystemResourceUtility
-				.getFileResource("xbrl/tifrs-20130331/XBRL_TW_Entry_Points/CI/CR/tifrs-ci-cr-2013-03-31.xsd");
+		File file = resourceLoader
+				.getResource("classpath:/xbrl/tifrs-20130331/XBRL_TW_Entry_Points/CI/CR/tifrs-ci-cr-2013-03-31.xsd").getFile();
+//		File file = SystemResourceUtility
+//				.getFileResource("xbrl/tifrs-20130331/XBRL_TW_Entry_Points/CI/CR/tifrs-ci-cr-2013-03-31.xsd");
 		Document document = DocumentUtility.parse(file);
-		
-		System.err.println(document.getDocumentElement());
+
+		System.err.println(document.getDocumentElement().getFirstChild());
 		
 		Assert.assertNotNull(document);
 

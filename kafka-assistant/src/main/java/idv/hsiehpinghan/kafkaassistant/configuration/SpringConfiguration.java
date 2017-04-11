99a9da6217aee773db343aa48bd7bda3628d19ed
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-04-11 16:10:36 +0800

    backup.

---
+++ kafka-assistant/src/main/java/idv/hsiehpinghan/kafkaassistant/configuration/SpringConfiguration.java
@@ -0,0 +1,80 @@
+package idv.hsiehpinghan.kafkaassistant.configuration;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration("kafkaAssistantSpringConfiguration")
+@PropertySource("classpath:/kafka_assistant.property")
+@ComponentScan(basePackages = { "idv.hsiehpinghan.kafkaassistant" })
+public class SpringConfiguration {
+
+	@Autowired
+	private Environment environment;
+
+//	@Bean
+//	public DataSource dataSource() throws PropertyVetoException {
+//		String driverClass = environment
+//				.getRequiredProperty("postgresql.driverClass");
+//		String jdbcUrl = environment.getRequiredProperty("postgresql.jdbcUrl");
+//		String user = environment.getRequiredProperty("postgresql.user");
+//		String password = environment
+//				.getRequiredProperty("postgresql.password");
+//		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+//		comboPooledDataSource.setDriverClass(driverClass);
+//		comboPooledDataSource.setJdbcUrl(jdbcUrl);
+//		comboPooledDataSource.setUser(user);
+//		comboPooledDataSource.setPassword(password);
+//		return comboPooledDataSource;
+//	}
+//
+//	@Bean
+//	public LocalSessionFactoryBean sessionFactory(DataSource dataSource)
+//			throws PropertyVetoException {
+//		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//		sessionFactory.setDataSource(dataSource);
+//		sessionFactory.setPackagesToScan(new String[] { environment
+//				.getRequiredProperty("hibernate.packagesToScan") });
+//		sessionFactory.setHibernateProperties(getHibernateProperties());
+//		return sessionFactory;
+//	}
+//
+//	@Bean
+//	public PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
+//		return new PersistenceExceptionTranslationPostProcessor();
+//	}
+
+	private Properties getHibernateProperties() {
+		Properties prop = new Properties();
+		prop.put("hibernate.default_schema",
+				environment.getRequiredProperty("hibernate.default_schema"));
+		prop.put("hibernate.dialect",
+				environment.getRequiredProperty("hibernate.dialect"));
+		prop.put("hibernate.format_sql",
+				environment.getRequiredProperty("hibernate.format_sql"));
+		prop.put("hibernate.hbm2ddl.auto",
+				environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		prop.put("hibernate.show_sql",
+				environment.getRequiredProperty("hibernate.show_sql"));
+		prop.put("hibernate.generate_statistics", environment
+				.getRequiredProperty("hibernate.generate_statistics"));
+		prop.put("hibernate.use_sql_comments",
+				environment.getRequiredProperty("hibernate.use_sql_comments"));
+		prop.put("hibernate.connection.isolation", environment
+				.getRequiredProperty("hibernate.connection.isolation"));
+		prop.put("hibernate.jdbc.fetch_size",
+				environment.getRequiredProperty("hibernate.jdbc.fetch_size"));
+		prop.put("hibernate.jdbc.batch_size",
+				environment.getRequiredProperty("hibernate.jdbc.batch_size"));
+		return prop;
+	}
+
+}

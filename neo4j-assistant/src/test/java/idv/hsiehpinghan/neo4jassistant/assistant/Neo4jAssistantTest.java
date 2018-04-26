d4d6e5f98ecd0708e7827bfe6c0964a4640f7705
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-04-26 15:55:46 +0800

    backup.

--- neo4j-assistant/src/test/java/idv/hsiehpinghan/neo4jassistant/assistant/Neo4jAssistantTest.java
+++ neo4j-assistant/src/test/java/idv/hsiehpinghan/neo4jassistant/assistant/Neo4jAssistantTest.java
@@ -1,9 +1,13 @@
 package idv.hsiehpinghan.neo4jassistant.assistant;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.types.Node;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
@@ -43,7 +47,11 @@ public class Neo4jAssistantTest extends AbstractTestNGSpringContextTests {
 
 	@Test(dependsOnMethods= {"create"})
 	public void retrieve() {
-		String statement = String.format("MATCH (n_0:%s)-[r_0:%s]-> (n_1:%s) WHERE r_0.property_key_0='property_value_0' and r_0.property_key_1='property_value_1' RETURN r_0.property_key_2", NODE_0_LABEL_1, RELATIONSHIP_LABEL_1, NODE_1_LABEL_1);
+		String statement = String.format(
+			"MATCH (n_0:%s)-[r_0:%s]-> (n_1:%s) " +
+			"WHERE r_0.property_key_0='property_value_0' and r_0.property_key_1='property_value_1' " +
+			"RETURN r_0.property_key_2",
+			NODE_0_LABEL_1, RELATIONSHIP_LABEL_1, NODE_1_LABEL_1);
 		StatementResult result = assistant.run(statement);
 		int j = 0;
 		while (result.hasNext()) {
@@ -54,7 +62,77 @@ public class Neo4jAssistantTest extends AbstractTestNGSpringContextTests {
 		}
 		Assert.assertEquals(j, 1);
 	}
+
+	@Test(dependsOnMethods= {"retrieve"})
+	public void update() {
+		final String NEW_PROPERTY_VALUE_0 = "property_value_a";
+		final String NEW_LABEL_0 = "new_label_0";
+		final String NEW_LABEL_1 = "new_label_1";
+		String statement = String.format(
+			"MATCH (n_0:%s)-[r_0:%s]-> (n_1:%s) " +
+			"WHERE r_0.property_key_0='property_value_0' and r_0.property_key_1='property_value_1' " + 
+			"SET n_0.property_key_0='%s' " +
+			"REMOVE n_0.property_key_1 " +
+			"SET n_0:%s:%s " +
+			"RETURN n_0",
+			NODE_0_LABEL_1, RELATIONSHIP_LABEL_1, NODE_1_LABEL_1, NEW_PROPERTY_VALUE_0, NEW_LABEL_0, NEW_LABEL_1);
+		StatementResult result = assistant.run(statement);
+		List<String> labelList = Arrays.asList(NODE_0_LABEL_1, NEW_LABEL_0, NEW_LABEL_1);
+		int i = 0;
+		while (result.hasNext()) {
+			Record record = result.next();
+			Assert.assertEquals(record.size(), 1);
+			Node node = record.get(0).asNode();
+			Assert.assertEquals(node.get("property_key_0").asString(), NEW_PROPERTY_VALUE_0);
+			Assert.assertTrue(node.get("property_key_1").isNull());
+			Iterable<String> labels = node.labels();
+			int j = 0;
+			for(String label : labels) {
+				labelList.contains(label);
+				++j;
+			}
+			Assert.assertEquals(j, 3);
+			++i;
+		}
+		Assert.assertEquals(i, 1);		
+	}
 	
+	@Test(dependsOnMethods= {"update"})
+	public void delete() {
+		final String NEW_PROPERTY_VALUE_0 = "property_value_a";
+		final String NEW_LABEL_0 = "new_label_0";
+		final String NEW_LABEL_1 = "new_label_1";
+		
+//		graphDb.execute("MATCH (company:Company)" +
+//		  " WHERE company.name='Baeldung'" +
+//		  " DELETE company");
+
+		String statement = String.format(
+			"MATCH (n_0:%s)-[r_0:%s]-> (n_1:%s) " +
+			"WHERE r_0.property_key_0='property_value_0' and r_0.property_key_1='property_value_1' " + 
+			"DELETE n_0 " +
+			"RETURN n_0",
+			NODE_0_LABEL_1, RELATIONSHIP_LABEL_1, NODE_1_LABEL_1);
+		StatementResult result = assistant.run(statement);
+		int i = 0;
+		while (result.hasNext()) {
+			Record record = result.next();
+			Assert.assertEquals(record.size(), 1);
+			Node node = record.get(0).asNode();
+			Assert.assertEquals(node.get("property_key_0").asString(), NEW_PROPERTY_VALUE_0);
+			Assert.assertTrue(node.get("property_key_1").isNull());
+			Iterable<String> labels = node.labels();
+			int j = 0;
+//			for(String label : labels) {
+//				labelList.contains(label);
+//				++j;
+//			}
+			Assert.assertEquals(j, 3);
+			++i;
+		}
+		Assert.assertEquals(i, 1);	
+	}
+
 //	@Test
 //	public void creatingNodeRelationshipNode() {
 //		String statement = "CREATE (node_0:lable_0 {property_key_0:'property_value_0', property_key_1:'property_value_1', property_key_2:'property_value_2'})-[relationship_0:label_1 {property_key_3:'property_value_3', property_key_4:'property_value_4', property_key_5:'property_value_5'}]->(node_1:lable_2 {property_key_6:'property_value_6', property_key_7:'property_value_7', property_key_8:'property_value_8'}) RETURN node_0,relationship_0,node_1";
@@ -111,12 +189,6 @@ public class Neo4jAssistantTest extends AbstractTestNGSpringContextTests {
 //	}
 	
 	private void createRelationship(String node_0_label, String relationship_label, String node_1_label) {
-//		String property_key_0 = "property_key_0";
-//		String property_value_0 = "property_value_0";
-//		String property_key_1 = "property_key_1";
-//		String property_value_1 = "property_value_1";
-//		String property_key_2 = "property_key_2";
-//		String property_value_2 = "property_value_2";
 		String statement = String.format("CREATE (node_0:%s)-[relationship_0:%s {%s:'%s', %s:'%s', %s:'%s'}]->(node_1:%s) RETURN node_0,relationship_0,node_1", node_0_label,
 				relationship_label, PROPERTY_KEY_0, PROPERTY_VALUE_0, PROPERTY_KEY_1, PROPERTY_VALUE_1, PROPERTY_KEY_2,
 				PROPERTY_VALUE_2, node_1_label);

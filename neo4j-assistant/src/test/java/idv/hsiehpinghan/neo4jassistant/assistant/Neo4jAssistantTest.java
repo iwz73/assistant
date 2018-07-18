9143f0247de35fff02d3ddb5ed1d1dce558df988
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-07-18 10:37:47 +0800

    backup.

--- neo4j-assistant/src/test/java/idv/hsiehpinghan/neo4jassistant/assistant/Neo4jAssistantTest.java
+++ neo4j-assistant/src/test/java/idv/hsiehpinghan/neo4jassistant/assistant/Neo4jAssistantTest.java
@@ -2,12 +2,10 @@ package idv.hsiehpinghan.neo4jassistant.assistant;
 
 import java.util.Date;
 
-import org.neo4j.driver.internal.value.NodeValue;
 import org.neo4j.driver.internal.value.RelationshipValue;
 import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.Value;
-import org.neo4j.driver.v1.types.Node;
 import org.neo4j.driver.v1.types.Relationship;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,8 +17,7 @@ import idv.hsiehpinghan.neo4jassistant.configuration.SpringConfiguration;
 
 @ContextConfiguration(classes = { SpringConfiguration.class })
 public class Neo4jAssistantTest extends AbstractTestNGSpringContextTests {
-	private final long NOW = new Date().getTime();
-	
+
 	@Autowired
 	private Neo4jAssistant assistant;
 	
@@ -37,6 +34,223 @@ public class Neo4jAssistantTest extends AbstractTestNGSpringContextTests {
 		createPath();
 	}
 	
+	@Test
+	public void merge() {
+		mergeNodeWithLabel();
+		mergeNodeWithProperty();
+		mergeNodeWithOnCreateAndOnMatch();
+		mergeRelationship();
+	}
+
+	@Test
+	public void set() {
+		setNodeProperty();
+		removeNodeProperty();
+		setNodeLabel();
+	}
+	
+	@Test
+	public void delete() {
+		deleteAllNode();
+		deleteMultipleNodes();
+		deleteSingleNode();
+	}
+	
+	private void deleteMultipleNodes() {
+		String createStatement = String.format("CREATE (n:l {p_0:'A', p_1:'B', p_2:'C'}) RETURN n");
+		StatementResult createResult = assistant.run(createStatement);
+		Assert.assertTrue(createResult.hasNext());
+		String detachDeleteStatement = String.format(
+			"MATCH (n:l {p_0:'A', p_1:'B', p_2:'C'}) " +
+			"DETACH DELETE n"
+		);
+		assistant.run(detachDeleteStatement);
+		String matchStatement = String.format(
+			"MATCH (n:l {p_0:'A', p_1:'B', p_2:'C'}) " +
+			"RETURN n"
+		);
+		StatementResult matchResult = assistant.run(matchStatement);
+		Assert.assertFalse(matchResult.hasNext());
+	}
+
+	private void deleteSingleNode() {
+		String createStatement = String.format("CREATE (n) RETURN n");
+		StatementResult createResult = assistant.run(createStatement);
+		Assert.assertTrue(createResult.hasNext());
+		long id = createResult.next().get(0).asNode().id();
+		String detachDeleteStatement = String.format(
+			"MATCH (n) " +
+			"WHERE ID(n) = %d " +
+			"DETACH DELETE n",
+			id
+		);
+		assistant.run(detachDeleteStatement);
+		String matchStatement = String.format(
+			"MATCH (n) " +
+			"WHERE ID(n) = %d " +
+			"RETURN n",
+			id
+		);
+		StatementResult matchResult = assistant.run(matchStatement);
+		Assert.assertFalse(matchResult.hasNext());
+	}
+
+	private void deleteAllNode() {
+		String createStatement = String.format("CREATE (n) RETURN n");
+		StatementResult createResult = assistant.run(createStatement);
+		Assert.assertTrue(createResult.hasNext());
+		String detachDeleteStatement = String.format("MATCH (n) DETACH DELETE n");
+		assistant.run(detachDeleteStatement);
+		String matchStatement = String.format(
+				"MATCH (n) " +
+				"RETURN n");
+		StatementResult matchResult = assistant.run(matchStatement);
+		Assert.assertFalse(matchResult.hasNext());
+	}
+
+	private void setNodeLabel() {
+		String createStatement = String.format("CREATE (n) RETURN n");
+		StatementResult createResult = assistant.run(createStatement);
+		Assert.assertTrue(createResult.hasNext());
+		long id = createResult.next().get(0).asNode().id();
+		String label_0 = "l_0";
+		String label_1 = "l_1";
+		String matchSetStatement = String.format(
+				"MATCH (n) WHERE ID(n) = %d " +
+				"SET n:%s:%s " +
+				"RETURN n", id, label_0, label_1);
+		StatementResult matchSetResult = assistant.run(matchSetStatement);
+		Assert.assertTrue(matchSetResult.hasNext());
+		int i = 0;
+		for(String label : matchSetResult.next().get(0).asNode().labels()) {
+			++i;
+		}
+		Assert.assertEquals(i, 2);
+	}
+
+	private void removeNodeProperty() {
+		String createStatement = String.format("CREATE (n {p:'A'}) RETURN n");
+		StatementResult createResult = assistant.run(createStatement);
+		Assert.assertTrue(createResult.hasNext());
+		long id = createResult.next().get(0).asNode().id();
+		String matchSetStatement = String.format(
+				"MATCH (n) WHERE ID(n) = %d " +
+				"SET n.p = NULL " +
+				"RETURN n", id);
+		StatementResult matchSetResult = assistant.run(matchSetStatement);
+		Assert.assertTrue(matchSetResult.hasNext());
+		int i = 0;
+		for(Value value : matchSetResult.next().get(0).asNode().values()) {
+			++i;
+		}
+		Assert.assertEquals(i, 0);
+	}
+
+	private void setNodeProperty() {
+		String createStatement = String.format("CREATE (n) RETURN n");
+		StatementResult createResult = assistant.run(createStatement);
+		Assert.assertTrue(createResult.hasNext());
+		long id = createResult.next().get(0).asNode().id();
+		String property_0 = "p_0";
+		String property_1 = "p_1";
+		String matchSetStatement = String.format(
+				"MATCH (n) WHERE ID(n) = %d " +
+				"SET n.p_0 = '%s', n.p_1 = '%s' " +
+				"RETURN n", id, property_0, property_1);
+		StatementResult matchSetResult = assistant.run(matchSetStatement);
+		Assert.assertTrue(matchSetResult.hasNext());
+		int i = 0;
+		for(Value value : matchSetResult.next().get(0).asNode().values()) {
+			++i;
+		}
+		Assert.assertEquals(i, 2);
+	}
+
+	private void mergeRelationship() {
+		final long NOW = new Date().getTime();
+		String label_0 = "l_0_" + NOW;
+		String label_1 = "l_1_" + NOW;
+		String property_0 = "p_0_" + NOW;
+		String property_1 = "p_1_" + NOW;
+		String createStatement = String.format("CREATE (n_0:%s {p_0:'%s'}), (n_1:%s {p_1:'%s'})", label_0, property_0, label_1, property_1);
+		assistant.run(createStatement);
+		String mergeStatement = String.format(
+			"MATCH (n_0:%s), (n_1:%s) " +
+			"WHERE n_0.p_0 = '%s' AND n_1.p_1 = '%s' " +
+			"MERGE (n_0)-[r:l_0]->(n_1) " +
+			"RETURN n_0, r, n_1", label_0, label_1, property_0, property_1);
+		assistant.run(mergeStatement);
+		StatementResult mergeResult = assistant.run(mergeStatement);
+		int i = 0;
+		while (mergeResult.hasNext()) {
+			Record record = mergeResult.next();
+			int size = record.size();
+			Assert.assertEquals(size, 3);
+			++i;
+		}
+		Assert.assertEquals(i, 1);		
+	}
+
+	private void mergeNodeWithLabel() {
+		final long NOW = new Date().getTime();
+		String label = "l_" + NOW;
+		String createStatement = String.format("CREATE (n:%s {p_0:'A'})", label);
+		assistant.run(createStatement);
+		String mergeStatement = String.format("MERGE (n:%s) RETURN n", label);
+		StatementResult mergeResult = assistant.run(mergeStatement);
+		int i = 0;
+		while (mergeResult.hasNext()) {
+			Record record = mergeResult.next();
+			int size = record.size();
+			Assert.assertEquals(size, 1);
+			for(Value value : record.get(0).asNode().values()) {
+				Assert.assertEquals(value.asString(), "A");
+			}
+			++i;
+		}
+		Assert.assertEquals(i, 1);		
+	}
+	
+	private void mergeNodeWithProperty() {
+		final long NOW = new Date().getTime();
+		String property = "p_" + NOW;
+		String createStatement = String.format("CREATE (n:l_0 {p_0:'%s'})", property);
+		assistant.run(createStatement);
+		String mergeStatement = String.format("MERGE (n {p_0:'%s'}) RETURN n", property);
+		StatementResult mergeResult = assistant.run(mergeStatement);
+		int i = 0;
+		while (mergeResult.hasNext()) {
+			Record record = mergeResult.next();
+			int size = record.size();
+			Assert.assertEquals(size, 1);
+			for(String label : record.get(0).asNode().labels()) {
+				Assert.assertEquals(label, "l_0");
+			}
+			++i;
+		}
+		Assert.assertEquals(i, 1);		
+	}	
+
+	private void mergeNodeWithOnCreateAndOnMatch() {
+		final long NOW = new Date().getTime();
+		String property = "p_" + NOW;
+		String mergeStatement = String.format(
+			"MERGE (n {p_0:'%s'}) " +
+			"ON CREATE SET n.isCreate = true " +
+			"ON MATCH SET n.isMatch = true " +
+			"RETURN n", property);
+		StatementResult mergeResult = assistant.run(mergeStatement);
+		int i = 0;
+		while (mergeResult.hasNext()) {
+			Record record = mergeResult.next();
+			int size = record.size();
+			Assert.assertEquals(size, 1);
+			Assert.assertTrue((boolean)record.get(0).asMap().get("isCreate"));
+			++i;
+		}
+		Assert.assertEquals(i, 1);		
+	}
+
 	private void createSingleNode() {
 		String createStatement = String.format("CREATE (n) RETURN n");
 		StatementResult createResult = assistant.run(createStatement);
@@ -142,6 +356,7 @@ public class Neo4jAssistantTest extends AbstractTestNGSpringContextTests {
 	}
 
 	private void createRelationshipBetweenExistingNode() {
+		final long NOW = new Date().getTime();
 		String property_0 = "p_0_" + NOW;
 		String property_1 = "p_1_" + NOW;
 		String createStatement = String.format("CREATE (n_0:l_0 {p_0:'%s'}), (n_1:l_1 {p_1:'%s'}) RETURN n_0, n_1", property_0, property_1);

23223006763ec144109941c4057abcfd9906ac33
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-10-03 05:51:34 +0800

    backup.

--- neo4j-assistant/src/test/java/idv/hsiehpinghan/neo4jassistant/assistant/Neo4jApocAssistantTest.java
+++ neo4j-assistant/src/test/java/idv/hsiehpinghan/neo4jassistant/assistant/Neo4jApocAssistantTest.java
@@ -1,5 +1,7 @@
 package idv.hsiehpinghan.neo4jassistant.assistant;
 
+import java.util.Map;
+
 import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.StatementResult;
 import org.neo4j.driver.v1.types.Node;
@@ -7,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import idv.hsiehpinghan.neo4jassistant.configuration.SpringConfiguration;
@@ -21,12 +22,127 @@ public class Neo4jApocAssistantTest extends AbstractTestNGSpringContextTests {
 	/**
 	 * if fail, rm -rf /tmp/neo4j/data/* and restart neo4j, and try again.
 	 */
-	@Test
+//	@Test
 	public void index() throws Exception {
 		initData();
 		indexAddNode();
-		indexNodes();		
+		indexNodes();
+		indexList();
 		indexRemove();
+	}
+
+//	@Test
+	public void help() throws Exception {
+		String name = "dijkstra";
+		// @formatter:off
+		String callStatement = String.format("CALL apoc.help('%s') ", name);
+		// @formatter:on
+		StatementResult callResult = assistant.read(callStatement);
+		int i = 0;
+		while (callResult.hasNext()) {
+			Record record = callResult.next();
+			++i;
+		}
+		Assert.assertTrue(0 < i);
+	}
+
+//	@Test
+	public void loadJson() throws Exception {
+		String url = "https://raw.githubusercontent.com/neo4j-contrib/neo4j-apoc-procedures/3.4/src/test/resources/person.json";
+		// @formatter:off
+		String callStatement = String.format("WITH '%s' AS url " + 
+			"CALL apoc.load.json(url) YIELD value as person " +
+			"MERGE (p:Person {name:person.name}) " +
+			"ON CREATE SET p.age = person.age, p.children = size(person.children) " + 
+			"RETURN p ", 
+			url);
+		// @formatter:on
+		StatementResult callResult = assistant.write(callStatement);
+		int i = 0;
+		while (callResult.hasNext()) {
+			Record record = callResult.next();
+			Node node = record.get(0).asNode();
+			Assert.assertEquals(node.get("name").asString(), "Michael");
+			Assert.assertEquals(node.get("age").asInt(), 41);
+			Assert.assertEquals(node.get("children").asInt(), 3);
+			++i;
+		}
+		Assert.assertEquals(i, 1);
+	}
+
+	@Test
+	public void spatial() throws Exception {
+		geocodeOnce();
+		geocode();
+		reverseGeocode();
+	}
+
+	private void geocode() throws Exception {
+		// @formatter:off
+		String callStatement = String.format(
+			"CALL apoc.spatial.geocode('中和水源路', 1) YIELD location\n" + 
+			"RETURN location ");
+		// @formatter:on
+		StatementResult callResult = assistant.run(callStatement);		
+		int i = 0;
+		while (callResult.hasNext()) {
+			Record record = callResult.next();
+			Map<String, Object> location = record.get(0).asMap();
+			Assert.assertTrue(location.get("description").toString().contains("新北市"));
+			++i;
+		}
+		Assert.assertEquals(i, 1);
+	}
+
+	private void geocodeOnce() throws Exception {
+		// @formatter:off
+		String callStatement = String.format(
+			"CALL apoc.spatial.geocodeOnce('中和水源路') YIELD location\n" + 
+			"RETURN location ");
+		// @formatter:on
+		StatementResult callResult = assistant.run(callStatement);		
+		int i = 0;
+		while (callResult.hasNext()) {
+			Record record = callResult.next();
+			Map<String, Object> location = record.get(0).asMap();
+			Assert.assertTrue(location.get("description").toString().contains("新北市"));
+			++i;
+		}
+		Assert.assertEquals(i, 1);
+	}
+
+	private void reverseGeocode() throws Exception {
+		// @formatter:off
+		String callStatement = String.format(
+			"CALL apoc.spatial.reverseGeocode(25.003548, 121.5058428) YIELD location " + 
+			"RETURN location ");
+		// @formatter:on
+		StatementResult callResult = assistant.run(callStatement);		
+		int i = 0;
+		while (callResult.hasNext()) {
+			Record record = callResult.next();
+			Map<String, Object> location = record.get(0).asMap();
+			Assert.assertTrue(location.get("description").toString().contains("新北市"));
+			++i;
+		}
+		Assert.assertEquals(i, 1);
+	}
+
+	private void indexList() throws Exception {
+		// @formatter:off
+		String callStatement = String.format(
+			"CALL apoc.index.list() ");
+		// @formatter:on
+		StatementResult callResult = assistant.run(callStatement);		
+		int i = 0;
+		while (callResult.hasNext()) {
+			Record record = callResult.next();
+			String indexName = String.format("Airport");			
+			if(record.get(1).asString().equals(indexName) == true) {
+				++i;
+			}
+		}
+		Assert.assertEquals(i, 1);
 	}
 
 	private void indexNodes() throws Exception {
@@ -80,45 +196,6 @@ public class Neo4jApocAssistantTest extends AbstractTestNGSpringContextTests {
 			if(record.get(1).asString().equals(indexName) == true) {
 				++i;
 			}
-		}
-		Assert.assertEquals(i, 1);
-	}
-
-	@Test
-	public void help() throws Exception {
-		String name = "dijkstra";
-		// @formatter:off
-		String callStatement = String.format("CALL apoc.help('%s') ", name);
-		// @formatter:on
-		StatementResult callResult = assistant.read(callStatement);
-		int i = 0;
-		while (callResult.hasNext()) {
-			Record record = callResult.next();
-			++i;
-		}
-		Assert.assertTrue(0 < i);
-	}
-
-	@Test
-	public void loadJson() throws Exception {
-		String url = "https://raw.githubusercontent.com/neo4j-contrib/neo4j-apoc-procedures/3.4/src/test/resources/person.json";
-		// @formatter:off
-		String callStatement = String.format("WITH '%s' AS url " + 
-			"CALL apoc.load.json(url) YIELD value as person " +
-			"MERGE (p:Person {name:person.name}) " +
-			"ON CREATE SET p.age = person.age, p.children = size(person.children) " + 
-			"RETURN p ", 
-			url);
-		// @formatter:on
-		StatementResult callResult = assistant.write(callStatement);
-		int i = 0;
-		while (callResult.hasNext()) {
-			Record record = callResult.next();
-			Node node = record.get(0).asNode();
-			Assert.assertEquals(node.get("name").asString(), "Michael");
-			Assert.assertEquals(node.get("age").asInt(), 41);
-			Assert.assertEquals(node.get("children").asInt(), 3);
-			++i;
 		}
 		Assert.assertEquals(i, 1);
 	}
